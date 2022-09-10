42f5ce186e0ec7622bda7a2a310ea8965f94c6c0
이진욱 <koownij@kakao.com>
2022-09-10 13:50:28 +0900

    chore-PLUB-94: buildSrc 관련 세팅

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,48 @@
+object Kotlin {
+    const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.KOTLIN_VERSION}"
+    const val COROUTINES_CORE =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLIN_COROUTINE}"
+    const val COROUTINES_ANDROID =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.KOTLIN_COROUTINE}"
+}
+
+
+object AndroidX {
+    const val APP_COMPAT = "androidx.appcompat:appcompat:${Versions.APP_COMPAT}"
+    const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+    const val CONSTRAINT_LAYOUT =
+        "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
+    const val LIFECYCLE_RUNTIME_KTX =
+        "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.LIFECYCLE_KTX}"
+    const val LIFECYCLE_VIEW_MODEL_KTX =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.VIEW_MODEL_KTX}"
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:${Versions.ACTIVITY_KTX}"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_KTX}"
+}
+
+object Google {
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+    const val HILT_CORE = "com.google.dagger:hilt-core:${Versions.HILT}"
+
+    const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+}
+
+object Libraries {
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+    const val RETROFIT_CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}"
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
+    const val OKHTTP_LOGGING_INTERCEPTOR =
+        "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
+
+    const val COIL = "io.coil-kt:coil-compose:${Versions.COIL}"
+}
+
+object UnitTest {
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+}
+
+object AndroidTest {
+    const val ANDROID_JUNIT = "junit:junit:${Versions.ANDROID_JUNIT}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,34 @@
+object Versions {
+    const val GRADLE = "7.1.2"
+
+    /* [ Kotlin ] */
+    const val KOTLIN_VERSION = "1.6.10"
+    const val KOTLIN_COROUTINE = "1.5.1"
+
+    /* [ Android ] */
+    const val APP_COMPAT = "1.5.1"
+    const val CONSTRAINT_LAYOUT = "2.1.4"
+    const val LIFECYCLE_KTX = "2.5.1"
+    const val VIEW_MODEL_KTX = "2.5.1"
+    const val ACTIVITY_KTX = "1.5.1"
+    const val FRAGMENT_KTX = "1.5.2"
+
+    const val CORE_KTX = "1.9.0"
+
+    /* [ Google ] */
+    const val HILT = "2.43.2"
+    const val MATERIAL = "1.6.1"
+
+    /* [ Libraries ] */
+    const val RETROFIT = "2.9.0"
+    const val OKHTTP = "4.10.0"
+
+    const val COIL = "1.4.0"
+
+    /* [ UnitTest ] */
+    const val JUNIT = "4.13.2"
+
+    /* [ AndroidTest ] */
+    const val ANDROID_JUNIT = "1.1.3"
+    const val ESPRESSO_CORE = "3.4.0"
+}
