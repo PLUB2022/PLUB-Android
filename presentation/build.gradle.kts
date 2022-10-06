25df353b13e5447a4bb31aa82ae8295df0d33050
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-10-07 00:18:23 +0900

    코멘트 수정

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -53,7 +53,5 @@ dependencies {
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
     kapt(Google.HILT_ANDROID_COMPILER)
 }
