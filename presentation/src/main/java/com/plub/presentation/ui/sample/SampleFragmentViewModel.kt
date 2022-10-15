7dbbda9440ba774bf0990dc50d5457c26b1c6b7b
이진욱 <koownij@kakao.com>
2022-10-15 22:32:14 +0900

    feat-103: jwt 저장 및 불러오기 기능 sample 추가

--- presentation/src/main/java/com/plub/presentation/ui/sample/SampleFragmentViewModel.kt
+++ presentation/src/main/java/com/plub/presentation/ui/sample/SampleFragmentViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.plub.domain.UiState
 import com.plub.domain.model.SampleLogin
+import com.plub.domain.repository.PlubJwtTokenRepository
 import com.plub.domain.successOrNull
 import com.plub.domain.usecase.TrySampleLoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,13 +14,39 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SampleFragmentViewModel @Inject constructor(
-    private val trySampleLoginUseCase: TrySampleLoginUseCase
+    private val trySampleLoginUseCase: TrySampleLoginUseCase,
+    private val plubJwtTokenRepository: PlubJwtTokenRepository
 ) : ViewModel() {
     private val _uiState = MutableStateFlow<UiState<SampleLogin>>(UiState.Loading)
     val uiState: StateFlow<UiState<SampleLogin>> = _uiState.asStateFlow()
 
+    val acTokenInput = MutableStateFlow("")
+    val reTokenInput = MutableStateFlow("")
+
+    private val _acToken = MutableStateFlow("")
+    val acToken: StateFlow<String> = _acToken.asStateFlow()
+
+    private val _reToken = MutableStateFlow("")
+    val reToken: StateFlow<String> = _reToken.asStateFlow()
+
     init {
         trySampleLogin()
+    }
+
+    fun saveAcToken() = viewModelScope.launch {
+        plubJwtTokenRepository.saveAccessToken(acTokenInput.value)
+    }
+
+    fun saveReToken() = viewModelScope.launch {
+        plubJwtTokenRepository.saveAccessTokenAndRefreshToken(acTokenInput.value, reTokenInput.value)
+    }
+
+    fun getAccessToken() = viewModelScope.launch {
+        _acToken.value = plubJwtTokenRepository.getAccessToken()
+    }
+
+    fun getRefreshToken() = viewModelScope.launch {
+        _reToken.value = plubJwtTokenRepository.getRefreshToken()
     }
 
     fun trySampleLogin() = viewModelScope.launch {
