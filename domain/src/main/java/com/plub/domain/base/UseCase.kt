0697062a645d4ee2994d5f6100b529fcc89f590f
hammond <kgbduswns@gmail.com>
2022-11-07 16:48:31 +0900

    feat-111: Base Usecase의 제네릭이 List일 경우를 고려하여 DomainModel 타입제한 제거

--- domain/src/main/java/com/plub/domain/base/UseCase.kt
+++ domain/src/main/java/com/plub/domain/base/UseCase.kt
@@ -4,7 +4,6 @@ import com.plub.domain.UiState
 import com.plub.domain.model.vo.login.SampleLogin
 import kotlinx.coroutines.flow.Flow
 
-abstract class UseCase<PARAM,MODEL:DomainModel> {
-
+abstract class UseCase<PARAM,MODEL> {
     abstract fun invoke(request:PARAM): Flow<UiState<MODEL>>
 }
