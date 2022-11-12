324cce74b697946ef66c488b1eedc342894740c7
이진욱 <koownij@kakao.com>
2022-11-12 12:13:14 +0900

    chore-103: PrefDataStoreRepository.kt 필요없는 메서드 제거

--- domain/src/main/java/com/plub/domain/repository/PrefDataStoreRepository.kt
+++ domain/src/main/java/com/plub/domain/repository/PrefDataStoreRepository.kt
@@ -4,15 +4,6 @@ import com.plub.domain.UiState
 import kotlinx.coroutines.flow.Flow
 
 interface PrefDataStoreRepository {
-    suspend fun getString(key: String): UiState<String?>
-
-    suspend fun getInt(key: String): Flow<Int?>
-
-    suspend fun getBoolean(key: String): Flow<Boolean?>
-
-    suspend fun setString(key: String, value: String)
-
-    suspend fun setInt(key: String, value: Int)
-
-    suspend fun setBoolean(key: String, value: Boolean)
+    fun getBoolean(key: String): Flow<UiState<Boolean?>>
+    fun setBoolean(key: String, value: Boolean): Flow<UiState<Nothing>>
 }
