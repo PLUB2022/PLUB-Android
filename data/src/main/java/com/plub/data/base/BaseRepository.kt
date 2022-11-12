d2964fdffe30d54c113b20472a895b57e7161cc3
이진욱 <koownij@kakao.com>
2022-11-12 12:15:14 +0900

    feat-103: DataStore Base 관련 로직 수정 및 추가 수정 : DataStore에서 값을 불러올 때 Flow를 반환하도록 수정 추가 : DataStore에 값을 저장하는 Base 로직 추가

--- data/src/main/java/com/plub/data/base/BaseRepository.kt
+++ data/src/main/java/com/plub/data/base/BaseRepository.kt
@@ -2,6 +2,8 @@ package com.plub.data.base
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import com.google.gson.Gson
 import com.plub.data.UiStateCallback
 import com.plub.domain.error.UiError
@@ -9,30 +11,34 @@ import com.plub.domain.UiState
 import com.plub.domain.base.DomainModel
 import com.plub.domain.result.Failure
 import com.plub.domain.result.StateResult
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import retrofit2.Response
 
 abstract class BaseRepository {
 
-    suspend fun <E:DataEntity,D:DomainModel>request(response:Response<E>, mapper: Mapper<E,D>, result: UiStateCallback<D>) {
+    suspend fun <E : DataEntity, D : DomainModel> request(
+        response: Response<E>,
+        mapper: Mapper<E, D>,
+        result: UiStateCallback<D>
+    ) {
         try {
-            when(response.isSuccessful) {
+            when (response.isSuccessful) {
                 true -> {
                     val entity = response.body() as E
                     val domainModel = mapper.mapFromEntity(entity)
 
-                    val stateResult:StateResult = when(entity.customCode == StateResult.SUCCEED_CODE) {
-                        true -> StateResult.Succeed
-                        false -> Failure.identifyFailure(entity.customCode)
-                    }
+                    val stateResult: StateResult =
+                        when (entity.customCode == StateResult.SUCCEED_CODE) {
+                            true -> StateResult.Succeed
+                            false -> Failure.identifyFailure(entity.customCode)
+                        }
 
-                    result.onSuccess(UiState.Success(domainModel,stateResult), entity.customCode)
+                    result.onSuccess(UiState.Success(domainModel, stateResult), entity.customCode)
                 }
                 false -> {
-                    val entity = Gson().fromJson(response.errorBody()?.string(), DataEntity::class.java)
-                    val error = UiError.identifyHttpError(response.code(),entity.customCode)
+                    val entity =
+                        Gson().fromJson(response.errorBody()?.string(), DataEntity::class.java)
+                    val error = UiError.identifyHttpError(response.code(), entity.customCode)
                     result.onError(UiState.Error(error))
                 }
             }
@@ -41,9 +47,23 @@ abstract class BaseRepository {
         }
     }
 
-    suspend fun <T> request(dataStore: DataStore<Preferences>, key: Preferences.Key<T>): UiState<T?> =
+    fun <T> request(dataStore: DataStore<Preferences>, key: Preferences.Key<T>): Flow<UiState<T?>> =
         dataStore.data
+            .onStart { UiState.Loading }
             .catch { UiState.Error(UiError.Invalided) }
             .map { preferences -> UiState.Success(preferences[key], StateResult.Succeed) }
-            .first()
+
+    fun <T> request(
+        dataStore: DataStore<Preferences>,
+        key: Preferences.Key<T>,
+        value: T
+    ): Flow<UiState<Nothing>> = flow {
+        try {
+            dataStore.edit { preferences ->
+                preferences[key] = value
+            }
+        } catch (e: Exception) {
+            emit(UiState.Error(UiError.Invalided))
+        }
+    }
 }
