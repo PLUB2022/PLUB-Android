bc99b9cab1cdf64cde315f4adb78f21f06b10dbd
이진욱 <koownij@kakao.com>
2022-11-12 12:15:52 +0900

    chore-103: DataStoreImpl 로직 수정

--- data/src/main/java/com/plub/data/repository/PrefDataStoreRepositoryImpl.kt
+++ data/src/main/java/com/plub/data/repository/PrefDataStoreRepositoryImpl.kt
@@ -5,41 +5,16 @@ import androidx.datastore.preferences.core.*
 import com.plub.data.base.BaseRepository
 import com.plub.domain.UiState
 import com.plub.domain.repository.PrefDataStoreRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import java.io.IOException
 import javax.inject.Inject
 
 class PrefDataStoreRepositoryImpl @Inject constructor(
     private val dataStorePreference: DataStore<Preferences>
 ) : PrefDataStoreRepository, BaseRepository() {
+    override fun getBoolean(key: String): Flow<UiState<Boolean?>> =
+        request(dataStorePreference, booleanPreferencesKey(key))
 
-    override suspend fun getString(key: String): UiState<String?> = request(dataStorePreference, stringPreferencesKey(key))
-
-    override suspend fun getInt(key: String): Flow<Int?> =
-        dataStorePreference.data.map { preferences -> preferences[intPreferencesKey(key)] }
-
-    override suspend fun getBoolean(key: String): Flow<Boolean?> =
-        dataStorePreference.data.map { preferences -> preferences[booleanPreferencesKey(key)] }
-
-    override suspend fun setString(key: String, value: String) {
-        dataStorePreference.edit { preferences ->
-            val a = stringPreferencesKey(key)
-            preferences[stringPreferencesKey(key)] = value
-        }
-    }
-
-    override suspend fun setInt(key: String, value: Int) {
-        dataStorePreference.edit { preferences ->
-            preferences[intPreferencesKey(key)] = value
-        }
-    }
-
-    override suspend fun setBoolean(key: String, value: Boolean) {
-        dataStorePreference.edit { preferences ->
-            preferences[booleanPreferencesKey(key)] = value
-        }
-    }
+    override fun setBoolean(key: String, value: Boolean): Flow<UiState<Nothing>> =
+        request(dataStorePreference, booleanPreferencesKey(key), value)
 }
