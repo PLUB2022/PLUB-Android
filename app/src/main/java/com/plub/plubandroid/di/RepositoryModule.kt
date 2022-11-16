6e774f56e7025c7819c9086ba559c423158186f3
이진욱 <koownij@kakao.com>
2022-11-16 20:24:12 +0900

    chore-105: 필요없는 코드 제거

--- app/src/main/java/com/plub/plubandroid/di/RepositoryModule.kt
+++ app/src/main/java/com/plub/plubandroid/di/RepositoryModule.kt
@@ -1,9 +1,7 @@
 package com.plub.plubandroid.di
 
 import com.plub.data.repository.IntroRepositoryImpl
-import com.plub.data.repository.SampleAccountImpl
 import com.plub.domain.repository.IntroRepository
-import com.plub.domain.repository.SampleAccountRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,8 +15,4 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun providesIntroRepository(repositoryImpl: IntroRepositoryImpl): IntroRepository
-
-    @Singleton
-    @Binds
-    abstract fun provideSampleRepository(repositoryImpl: SampleAccountImpl) : SampleAccountRepository
 }
