7ad77d9b4557ad19ec58742868fb35a64ec978fa
koownij <koownij@kakao.com>
2022-11-26 15:41:16 +0900

    chore-105: 기존 PlubJwtTokenData를 PlubJwtTokenResponse로 변경

--- data/src/main/java/com/plub/data/dto/sample/PlubJwtTokenResponse.kt
+++ data/src/main/java/com/plub/data/dto/sample/PlubJwtTokenResponse.kt
@@ -1,17 +1,12 @@
-package com.plub.data.model
+package com.plub.data.dto.sample
 
 import com.google.gson.annotations.SerializedName
 import com.plub.data.base.DataDto
 
 data class PlubJwtTokenResponse(
-    @SerializedName("data")
-    val data : PlubJwtTokenData?
-):DataDto
-
-data class PlubJwtTokenData(
     @SerializedName("accessToken")
     val accessToken : String,
 
     @SerializedName("refreshToken")
     val refreshToken : String
-)
+):DataDto
--- data/src/main/java/com/plub/data/mapper/PlubJwtTokenMapper.kt
+++ data/src/main/java/com/plub/data/mapper/PlubJwtTokenMapper.kt
@@ -1,7 +1,7 @@
 package com.plub.data.mapper
 
 import com.plub.data.base.Mapper
-import com.plub.data.model.PlubJwtTokenResponse
+import com.plub.data.dto.sample.PlubJwtTokenResponse
 import com.plub.domain.model.vo.jwt_token.PlubJwtTokenData
 import com.plub.domain.model.vo.jwt_token.PlubJwtTokenVo
 
@@ -9,7 +9,7 @@ object PlubJwtTokenMapper: Mapper.ResponseMapper<PlubJwtTokenResponse, PlubJwtTo
 
     override fun mapDtoToModel(type: PlubJwtTokenResponse?): PlubJwtTokenVo {
         return type.run {
-            PlubJwtTokenVo(PlubJwtTokenData(this?.data?.accessToken ?: "", this?.data?.refreshToken ?: ""))
+            PlubJwtTokenVo(PlubJwtTokenData(this?.accessToken ?: "", this?.refreshToken ?: ""))
         }
     }
 }
