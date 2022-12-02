c46ae9b348e3bea1ca68b3e1d9da666552474dc2
koownij <koownij@kakao.com>
2022-12-02 13:18:23 +0900

    feat-120: 플러빙 만들기 - 카테고리 설정, parentViewModel 연동 후 다음 버튼 클릭 이벤트 구현

--- presentation/src/main/java/com/plub/presentation/ui/createGathering/selectPlubCategory/SelectPlubCategoryFragment.kt
+++ presentation/src/main/java/com/plub/presentation/ui/createGathering/selectPlubCategory/SelectPlubCategoryFragment.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.viewModels
 import com.plub.domain.model.state.PageState
 import com.plub.presentation.base.BaseFragment
 import com.plub.presentation.databinding.FragmentSelectPlubCategoryBinding
+import com.plub.presentation.ui.createGathering.CreateGatheringViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -12,10 +13,12 @@ class SelectPlubCategoryFragment : BaseFragment<FragmentSelectPlubCategoryBindin
     FragmentSelectPlubCategoryBinding::inflate
 ) {
     override val viewModel: SelectPlubCategoryViewModel by viewModels()
+    private val parentViewModel: CreateGatheringViewModel by viewModels({requireParentFragment()})
 
     override fun initView() {
         binding.apply {
             vm = viewModel
+            parentVm = parentViewModel
         }
     }
 }
