8c8342636a6c7b437ac499256119ed29cec1f506
koownij <koownij@kakao.com>
2022-12-15 20:03:14 +0900

    feat-120: 플러빙 모임 생성 - 목표, 소개글, 사진 Fragment PageAdapter 연동

--- domain/src/main/java/com/plub/domain/model/enums/CreateGatheringPageType.kt
+++ domain/src/main/java/com/plub/domain/model/enums/CreateGatheringPageType.kt
@@ -1,7 +1,7 @@
 package com.plub.domain.model.enums
 
 enum class CreateGatheringPageType(val idx: Int) {
-    SELECT_PLUB_CATEGORY(0), GATHERING_TITLE_AND_NAME(1)
+    SELECT_PLUB_CATEGORY(0), GATHERING_TITLE_AND_NAME(1), GOAL_INTRODUCE_PICTURE(2)
 
 
 }
--- presentation/src/main/java/com/plub/presentation/ui/createGathering/adapter/FragmentCreateGatheringPagerAdapter.kt
+++ presentation/src/main/java/com/plub/presentation/ui/createGathering/adapter/FragmentCreateGatheringPagerAdapter.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.plub.domain.model.enums.CreateGatheringPageType
 import com.plub.presentation.ui.createGathering.gatheringTitleAndName.CreateGatheringTitleAndNameFragment
+import com.plub.presentation.ui.createGathering.goalAndIntroduceAndPicture.CreateGatheringGoalAndIntroduceAndPictureFragment
 import com.plub.presentation.ui.createGathering.selectPlubCategory.SelectPlubCategoryFragment
 
 class FragmentCreateGatheringPagerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
@@ -13,6 +14,7 @@ class FragmentCreateGatheringPagerAdapter(fragment: Fragment): FragmentStateAdap
     override fun createFragment(position: Int): Fragment = when(position) {
         CreateGatheringPageType.SELECT_PLUB_CATEGORY.idx -> SelectPlubCategoryFragment()
         CreateGatheringPageType.GATHERING_TITLE_AND_NAME.idx -> CreateGatheringTitleAndNameFragment()
+        CreateGatheringPageType.GOAL_INTRODUCE_PICTURE.idx -> CreateGatheringGoalAndIntroduceAndPictureFragment()
         else -> throw IllegalAccessException()
     }
 }
