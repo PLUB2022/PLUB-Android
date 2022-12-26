23fc70aa30145d4d0a80a2ec8c437651465865f8
koownij <koownij@kakao.com>
2022-12-26 22:00:29 +0900

    feat-120: ImageUtil, IntentUtil 추가

---
+++ presentation/src/main/java/com/plub/presentation/util/ImageUtil.kt
@@ -0,0 +1,24 @@
+package com.plub.presentation.util
+
+import android.net.Uri
+import android.provider.MediaStore
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ImageUtil @Inject constructor(private val resourceProvider: ResourceProvider) {
+    private fun isFailGetColumnIndex(idx: Int) = idx == -1
+
+    fun getRealPathFromURI(uri: Uri): String {
+        var columnIndex = -1
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = resourceProvider.getCursor(uri, proj, null, null, null)
+        if (cursor != null && cursor.moveToFirst()) {
+            columnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+        }
+        if(isFailGetColumnIndex(columnIndex)) return ""
+        val result = cursor!!.getString(columnIndex) // columnIndex에 Path가 존재함
+        cursor.close()
+        return result
+    }
+}
