9a95503ece36200db2ab6745d67b847b9374aa5f
koownij <koownij@kakao.com>
2022-12-26 22:00:07 +0900

    feat-120: 플러빙 모임 생성 - 모임 이미지 추가 기능 완료

--- presentation/src/main/java/com/plub/presentation/ui/createGathering/goalAndIntroduceAndPicture/CreateGatheringGoalAndIntroduceAndPictureFragment.kt
+++ presentation/src/main/java/com/plub/presentation/ui/createGathering/goalAndIntroduceAndPicture/CreateGatheringGoalAndIntroduceAndPictureFragment.kt
@@ -1,11 +1,18 @@
 package com.plub.presentation.ui.createGathering.goalAndIntroduceAndPicture
 
+import android.content.Intent
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import com.plub.domain.model.state.CreateGatheringGoalAndIntroduceAndPicturePageState
-import com.plub.domain.model.state.PageState
+import com.plub.domain.model.state.CreateGatheringTitleAndNamePageState
 import com.plub.presentation.base.BaseFragment
 import com.plub.presentation.databinding.FragmentCreateGatheringGoalAndIntroduceAndPictureBinding
+import com.plub.presentation.ui.createGathering.CreateGatheringViewModel
+import com.plub.presentation.util.IntentUtil
+import com.plub.presentation.util.PermissionManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CreateGatheringGoalAndIntroduceAndPictureFragment :
@@ -14,9 +21,39 @@ class CreateGatheringGoalAndIntroduceAndPictureFragment :
             CreateGatheringGoalAndIntroduceAndPicturePageState,
             CreateGatheringGoalAndIntroduceAndPictureViewModel>
         (FragmentCreateGatheringGoalAndIntroduceAndPictureBinding::inflate) {
+
     override val viewModel: CreateGatheringGoalAndIntroduceAndPictureViewModel by viewModels()
+    private val parentViewModel: CreateGatheringViewModel by viewModels({requireParentFragment()})
 
     override fun initView() {
+        with(binding) {
+            vm = viewModel
+            parentVm = parentViewModel
+        }
+    }
 
+    override fun initState() {
+        super.initState()
+
+        repeatOnStarted(viewLifecycleOwner) {
+            launch {
+                viewModel.getImageFromGallery.collect {
+                    PermissionManager.createGetImagePermission {
+                        getImageFromGallery()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getImageFromGallery() {
+        val intent = IntentUtil.getSingleImageIntent()
+        gatheringImageResult.launch(intent)
+    }
+
+    private val gatheringImageResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        viewModel.proceedActivityResult(it)
     }
 }
--- presentation/src/main/java/com/plub/presentation/ui/createGathering/goalAndIntroduceAndPicture/CreateGatheringGoalAndIntroduceAndPictureViewModel.kt
+++ presentation/src/main/java/com/plub/presentation/ui/createGathering/goalAndIntroduceAndPicture/CreateGatheringGoalAndIntroduceAndPictureViewModel.kt
@@ -1,15 +1,44 @@
 package com.plub.presentation.ui.createGathering.goalAndIntroduceAndPicture
 
+import android.app.Activity.RESULT_OK
+import androidx.activity.result.ActivityResult
+import androidx.lifecycle.viewModelScope
 import com.plub.domain.model.state.CreateGatheringGoalAndIntroduceAndPicturePageState
 import com.plub.domain.model.state.PageState
 import com.plub.presentation.base.BaseViewModel
+import com.plub.presentation.util.ImageUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
-class CreateGatheringGoalAndIntroduceAndPictureViewModel @Inject constructor() :
+class CreateGatheringGoalAndIntroduceAndPictureViewModel @Inject constructor(
+    private val imageUtil: ImageUtil
+) :
     BaseViewModel<CreateGatheringGoalAndIntroduceAndPicturePageState>(
         CreateGatheringGoalAndIntroduceAndPicturePageState()
     ) {
+    private val _getImageFromGallery = MutableSharedFlow<Unit>(0, 1, BufferOverflow.DROP_OLDEST)
+    val getImageFromGallery: SharedFlow<Unit> = _getImageFromGallery.asSharedFlow()
 
+    fun onClickAddSingleImageButton() {
+        viewModelScope.launch {
+            _getImageFromGallery.emit(Unit)
+        }
+    }
+
+    fun proceedActivityResult(result: ActivityResult) {
+        if (result.resultCode == RESULT_OK) {
+            result.data?.data?.let { uri ->
+                updateUiState { uiState ->
+                    uiState.copy(gatheringPicture = File(imageUtil.getRealPathFromURI(uri)))
+                }
+            }
+        }
+    }
 }
