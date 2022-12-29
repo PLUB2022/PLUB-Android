98da12a320dd0af8fe91c63ba00b0e4cad5eaf8a
koownij <koownij@kakao.com>
2022-12-30 00:02:24 +0900

    feat-120: 커스텀 icon edittext 추가

---
+++ presentation/src/main/java/com/plub/presentation/ui/custom/IconEditText.kt
@@ -0,0 +1,42 @@
+package com.plub.presentation.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.plub.presentation.R
+import com.plub.presentation.databinding.CustomViewFixedTextWidthCheckBoxBinding
+import com.plub.presentation.databinding.CustomViewIconEditTextBinding
+import com.plub.presentation.util.dp
+
+class IconEditText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding: CustomViewIconEditTextBinding
+    private val iconResourceId: Int
+    private val editTextClickable: Boolean
+    val editText: EditText
+
+    init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = DataBindingUtil.inflate(inflater, R.layout.custom_view_icon_edit_text, this, true)
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.IconEditText, defStyleAttr, 0)
+
+        iconResourceId = typedArray.getResourceId(R.styleable.IconEditText_icon, R.drawable.icon_location)
+        binding.imageViewIc.setImageResource(iconResourceId)
+
+        editTextClickable = typedArray.getBoolean(R.styleable.IconEditText_editTextClickable, true)
+        binding.editText.isClickable = editTextClickable
+
+        editText = binding.editText
+
+        typedArray.recycle()
+    }
+}
