2861c48920532222f88152edf5fc65e90bb50e0a
koownij <koownij@kakao.com>
2022-12-29 19:44:53 +0900

    feat-120: 플러빙 모임 생성 - 요일, 온/오프, 장소 - 요일 선택 로직 구현

--- presentation/src/main/java/com/plub/presentation/ui/createGathering/dayAndOnOfflineAndLocation/CreateGatheringDayAndOnOfflineAndLocationViewModel.kt
+++ presentation/src/main/java/com/plub/presentation/ui/createGathering/dayAndOnOfflineAndLocation/CreateGatheringDayAndOnOfflineAndLocationViewModel.kt
@@ -5,6 +5,7 @@ import com.plub.domain.model.state.CreateGatheringDayAndOnOfflineAndLocationPage
 import com.plub.presentation.base.BaseViewModel
 import com.plub.presentation.util.PlubLogger
 import com.plub.presentation.util.addOrRemoveElementAfterReturnNewHashSet
+import com.plub.presentation.util.removeElementAfterReturnNewHashSet
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -22,10 +23,27 @@ class CreateGatheringDayAndOnOfflineAndLocationViewModel @Inject constructor() :
         fun onClickCheckBox(element: String): Void? {
             updateUiState { uiState ->
                 uiState.copy(
-                    gatheringDays = uiState.gatheringDays.addOrRemoveElementAfterReturnNewHashSet(element)
+                    gatheringDays = uiState.gatheringDays
+                        .addOrRemoveElementAfterReturnNewHashSet(element)
+                        .removeElementAfterReturnNewHashSet(DaysType.ALL.value)
                 )
             }
             PlubLogger.logD("gatheringDays = ${uiState.value.gatheringDays}")
             return null
         }
+
+    /**
+     * https://stackoverflow.com/questions/60005152/cannot-use-same-bindingadapter-on-two-different-views
+     * Unit으로 할 경우 DataBinding Complier가 올바르지 않은 java code를 생성함.
+     * 따라서 Void를 사용
+     */
+    fun onClickAllCheckBox(): Void? {
+        updateUiState { uiState ->
+            uiState.copy(
+                gatheringDays = hashSetOf(DaysType.ALL.value)
+            )
+        }
+        PlubLogger.logD("gatheringDays = ${uiState.value.gatheringDays}")
+        return null
+    }
 }
