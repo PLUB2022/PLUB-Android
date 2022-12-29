44a493a0c3a2e6e44332d80f3433fddf3a29f218
hammond <kgbduswns@gmail.com>
2022-12-30 01:13:55 +0900

    fix-111: 블랭크 체크하는 정규식 규칙 수정

--- domain/src/main/java/com/plub/domain/RegexUtil.kt
+++ domain/src/main/java/com/plub/domain/RegexUtil.kt
@@ -4,7 +4,7 @@ import java.util.regex.Pattern
 
 object RegexUtil {
     private const val NON_SPECIAL_CHARACTER = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\\s]*$"
-    private const val BLANK_CHARACTER = "[\\s]"
+    private const val BLANK_CHARACTER = "(.*)\\s(.*)"
 
     fun hasBlackCharacter(str:String):Boolean = Pattern.matches(BLANK_CHARACTER, str)
     fun hasSpecialCharacter(str:String):Boolean = !Pattern.matches(NON_SPECIAL_CHARACTER, str)
