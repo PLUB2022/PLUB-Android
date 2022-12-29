c5ba03c4a222c41ea5345f7521b095f8f36a398a
koownij <koownij@kakao.com>
2022-12-29 19:35:41 +0900

    feat-120: HashSet 관련 extensions 추가

--- presentation/src/main/java/com/plub/presentation/util/Extensions.kt
+++ presentation/src/main/java/com/plub/presentation/util/Extensions.kt
@@ -4,3 +4,25 @@ import android.content.res.Resources
 
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+fun <T> HashSet<T>.removeElementAfterReturnNewHashSet(element: T): HashSet<T> {
+    val tempHashSet = this.toHashSet()
+    tempHashSet.remove(element)
+    return tempHashSet
+}
+
+fun <T> HashSet<T>.addElementAfterReturnNewHashSet(element: T): HashSet<T> {
+    val tempHashSet = this.toHashSet()
+    tempHashSet.add(element)
+    return tempHashSet
+}
+
+/**
+ *  element가 존재하는 경우 remove, 존재하지 않는 경우 add
+ */
+fun <T> HashSet<T>.addOrRemoveElementAfterReturnNewHashSet(element: T): HashSet<T> {
+    return if (element in this)
+        removeElementAfterReturnNewHashSet(element)
+    else
+        addElementAfterReturnNewHashSet(element)
+}
