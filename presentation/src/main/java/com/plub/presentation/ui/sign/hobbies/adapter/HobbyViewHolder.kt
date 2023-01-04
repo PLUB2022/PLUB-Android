d15779f7695d5da972b013a0b334d2975d389471
hammond <kgbduswns@gmail.com>
2023-01-04 17:37:16 +0900

    feat-111: 관심사 카테고리 어댑터, 뷰홀더 생성

---
+++ presentation/src/main/java/com/plub/presentation/ui/sign/hobbies/adapter/HobbyViewHolder.kt
@@ -0,0 +1,65 @@
+package com.plub.presentation.ui.sign.hobbies.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.plub.domain.model.vo.common.HobbyVo
+import com.plub.presentation.R
+import com.plub.presentation.databinding.IncludeItemHobbyBinding
+import com.plub.presentation.ui.common.GridSpaceDecoration
+import com.plub.presentation.util.GlideUtil
+import com.plub.presentation.util.dp
+
+class HobbyViewHolder(
+    private val binding: IncludeItemHobbyBinding,
+    private val listener: HobbiesAdapter.Delegate
+) : RecyclerView.ViewHolder(binding.root), HobbiesAdapter.SubListener {
+
+    companion object {
+        private const val TOTAL_SPAN_SIZE = 4
+        private const val ITEM_SPAN_SIZE = 1
+        private const val ITEM_SPACE = 8
+    }
+
+    private var vo: HobbyVo? = null
+    private val listAdapter = SubHobbiesAdapter(listener)
+
+    init {
+        binding.imageViewExpand.setOnClickListener {
+            vo?.let {
+                listener.onClickExpand(it.id)
+            }
+        }
+
+        binding.recyclerViewSubHobbies.apply {
+            layoutManager = GridLayoutManager(context,TOTAL_SPAN_SIZE).apply {
+                spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                    override fun getSpanSize(position: Int): Int {
+                        return ITEM_SPAN_SIZE
+                    }
+                }
+            }
+            addItemDecoration(GridSpaceDecoration(TOTAL_SPAN_SIZE, ITEM_SPACE.dp, false))
+            adapter = listAdapter
+        }
+    }
+
+    fun bind(item: HobbyVo) {
+        vo = item
+        binding.apply {
+            GlideUtil.loadImage(root.context, item.icon, imageViewIcon)
+            textViewName.text = item.name
+            val expandImageRes = if (item.isExpand) R.drawable.ic_arrow_over else R.drawable.ic_arrow_under
+            imageViewExpand.setImageResource(expandImageRes)
+            recyclerViewSubHobbies.visibility = if (item.isExpand) View.VISIBLE else View.GONE
+            listAdapter.submitList(item.subHobbies)
+        }
+    }
+
+    override fun onNotifySubItemChange(parentId: Int, subId: Int) {
+        vo?.let {
+            if(parentId != it.id) return
+            listAdapter.updateOnClick(subId)
+        }
+    }
+}
---
+++ presentation/src/main/java/com/plub/presentation/ui/sign/hobbies/adapter/SubHobbyViewHolder.kt
@@ -0,0 +1,38 @@
+package com.plub.presentation.ui.sign.hobbies.adapter
+
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.plub.domain.model.vo.common.SelectedHobbyVo
+import com.plub.domain.model.vo.common.SubHobbyVo
+import com.plub.presentation.R
+import com.plub.presentation.databinding.IncludeItemSubHobbyBinding
+
+class SubHobbyViewHolder(
+    private val binding: IncludeItemSubHobbyBinding,
+    private val listener: HobbiesAdapter.Delegate,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private var vo: SubHobbyVo? = null
+    private var isClicked:Boolean = false
+
+    init {
+        binding.root.setOnClickListener {
+            vo?.let {
+                listener.onClickSubHobby(isClicked, SelectedHobbyVo(it.parentHobbyId, it.id))
+            }
+        }
+    }
+
+    fun bind(item: SubHobbyVo) {
+        vo = item
+        binding.apply {
+            isClicked = listener.selectedList.contains(SelectedHobbyVo(item.parentHobbyId, item.id))
+            val textColorRes = if(isClicked) R.color.white else R.color.color_8c8c8c
+            val backgroundRes = if(isClicked) R.drawable.bg_rectangle_filled_5f5ff9_radius_8 else R.drawable.bg_rectangle_empty_8c8c8c_radius_8
+
+            textViewHobby.text = item.name
+            textViewHobby.setTextColor(ContextCompat.getColor(root.context,textColorRes))
+            root.setBackgroundResource(backgroundRes)
+        }
+    }
+}
