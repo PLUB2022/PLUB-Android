74dd707d02f74d51be63a93b0a77e891e1d16275
hammond <kgbduswns@gmail.com>
2023-01-06 03:49:30 +0900

    feat-111: DateFormatUtil 생성

---
+++ data/src/main/java/com/plub/data/util/DateFormatUtil.kt
@@ -0,0 +1,12 @@
+package com.plub.data.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateFormatUtil {
+    private val signUpFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
+
+    fun getSignUpBirthday(birthday:Calendar):String {
+        return signUpFormat.format(birthday)
+    }
+}
