806a8faad4d9bbfe4f6b387a0ef1badb9ea760d1
koownij <koownij@kakao.com>
2023-01-06 17:04:27 +0900

    chore-120: 누락된 파일 컴파일

---
+++ domain/src/main/java/com/plub/domain/model/state/createGathering/CreateGatheringQuestionPageState.kt
@@ -0,0 +1,7 @@
+package com.plub.domain.model.state.createGathering
+
+import com.plub.domain.model.state.PageState
+
+data class CreateGatheringQuestionPageState(
+    val temp: String = ""
+): PageState
