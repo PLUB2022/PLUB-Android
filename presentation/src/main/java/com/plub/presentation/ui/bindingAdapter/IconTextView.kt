27d877e88292eacbfcd1e1be58d8885433a646d0
koownij <koownij@kakao.com>
2023-01-10 14:46:38 +0900

    chore-120: IconTextView -> IconTextViewSmall 이름 변경

--- presentation/src/main/java/com/plub/presentation/ui/bindingAdapter/IconTextView.kt
+++ presentation/src/main/java/com/plub/presentation/ui/bindingAdapter/IconTextView.kt
@@ -1,15 +1,12 @@
 package com.plub.presentation.ui.bindingAdapter
 
-import android.view.KeyEvent
-import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.plub.domain.model.vo.kakaoLocation.KakaoLocationInfoDocumentVo
 import com.plub.presentation.R
-import com.plub.presentation.ui.custom.IconTextView
+import com.plub.presentation.ui.custom.IconTextViewSmall
 
-@BindingAdapter("iconTextViewTextLocation")
-fun IconTextView.bindIconTextViewTextLocation(data: KakaoLocationInfoDocumentVo?) {
+@BindingAdapter("iconTextViewSmallTextLocation")
+fun IconTextViewSmall.bindIconTextViewSmallTextLocation(data: KakaoLocationInfoDocumentVo?) {
     if(data == null)
         textView.text =
             context.getString(R.string.create_gathering_day_and_on_offline_and_location_gathering_location_hint)
--- presentation/src/main/java/com/plub/presentation/ui/custom/IconTextViewSmall.kt
+++ presentation/src/main/java/com/plub/presentation/ui/custom/IconTextViewSmall.kt
@@ -9,7 +9,7 @@ import androidx.databinding.DataBindingUtil
 import com.plub.presentation.R
 import com.plub.presentation.databinding.CustomViewIconTextViewBinding
 
-class IconTextView @JvmOverloads constructor(
+class IconTextViewSmall @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
@@ -25,12 +25,12 @@ class IconTextView @JvmOverloads constructor(
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         binding = DataBindingUtil.inflate(inflater, R.layout.custom_view_icon_text_view, this, true)
 
-        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.IconTextView, defStyleAttr, 0)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.IconTextViewSmall, defStyleAttr, 0)
 
-        iconResourceId = typedArray.getResourceId(R.styleable.IconTextView_iconTextViewIcon, R.drawable.ic_location)
+        iconResourceId = typedArray.getResourceId(R.styleable.IconTextViewSmall_iconTextViewSmallIcon, R.drawable.ic_location)
         binding.imageViewIc.setImageResource(iconResourceId)
 
-        text = typedArray.getString(R.styleable.IconTextView_iconTextViewText)
+        text = typedArray.getString(R.styleable.IconTextViewSmall_iconTextViewSmallText)
 
         text?.apply {
             binding.textView.text = text
