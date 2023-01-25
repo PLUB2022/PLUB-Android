e6b73f63e02ddffc963ebd93a97c70305ad8c7f8
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-01-25 22:50:26 +0900

    feat-104 : 모집 상세 프로필 추가

---
+++ presentation/src/main/java/com/plub/presentation/ui/home/adapter/DetailRecruitProfileAdapter.kt
@@ -0,0 +1,39 @@
+package com.plub.presentation.ui.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.plub.domain.model.vo.home.recruitdetailvo.RecruitDetailJoinedAccountsListVo
+import com.plub.presentation.databinding.IncludeItemCircleProfileBinding
+import com.plub.presentation.ui.home.adapter.viewholder.DetailRecruitProfileViewHolder
+
+class DetailRecruitProfileAdapter(private val listener : DetailProfileDegelate) : ListAdapter<RecruitDetailJoinedAccountsListVo, RecyclerView.ViewHolder>(
+    DetailRecruitProfileDiffCallBack()
+){
+    interface DetailProfileDegelate{
+        fun onProfileClick(accountId : Int)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is DetailRecruitProfileViewHolder -> {
+                if (position < 8){
+                    holder.bind(currentList[position], currentList.size, position + 1)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = IncludeItemCircleProfileBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DetailRecruitProfileViewHolder(binding, listener)
+    }
+
+}
+
+class DetailRecruitProfileDiffCallBack : DiffUtil.ItemCallback<RecruitDetailJoinedAccountsListVo>() {
+    override fun areItemsTheSame(oldItem: RecruitDetailJoinedAccountsListVo, newItem: RecruitDetailJoinedAccountsListVo): Boolean = oldItem == newItem
+    override fun areContentsTheSame(oldItem: RecruitDetailJoinedAccountsListVo, newItem: RecruitDetailJoinedAccountsListVo): Boolean = oldItem == newItem
+}
