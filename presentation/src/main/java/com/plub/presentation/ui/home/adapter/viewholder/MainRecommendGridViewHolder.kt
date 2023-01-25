ee9deced0d8bff98681f0e2eddb2ad873ac10061
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-01-25 22:23:11 +0900

    refactor-104 : 뷰 바인딩 코드 추가

--- presentation/src/main/java/com/plub/presentation/ui/home/adapter/viewholder/MainRecommendGridViewHolder.kt
+++ presentation/src/main/java/com/plub/presentation/ui/home/adapter/viewholder/MainRecommendGridViewHolder.kt
@@ -2,8 +2,10 @@ package com.plub.presentation.ui.home.adapter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
 import com.plub.domain.model.vo.home.recommendationgatheringvo.RecommendationGatheringResponseContentListVo
+import com.plub.presentation.R
 import com.plub.presentation.databinding.IncludeItemRecommendGatheringGridBinding
 import com.plub.presentation.ui.home.adapter.MainRecommendGridAdapter
+import com.plub.presentation.util.GlideUtil
 
 class MainRecommendGridViewHolder(
     private val binding: IncludeItemRecommendGatheringGridBinding,
@@ -16,6 +18,14 @@ class MainRecommendGridViewHolder(
                 listener.onClick(item.plubbingId)
             }
 
+            GlideUtil.loadImage(root.context, item.mainImage, imageViewRecommendGrid)
+            if(item.isBookmarked){
+                imageBtnBookmark.setImageResource(R.drawable.ic_bookmark_checked)
+            }
+            textViewPlubbingGatheringName.text = item.title
+            //textViewLocation.text =
+            //textViewPeople.text = "모집 인원 ${item.maxAccountNum}"
+            //textViewDate.text = "매주 ~
         }
     }
 }
--- presentation/src/main/java/com/plub/presentation/ui/home/adapter/viewholder/MainRecommendListViewHolder.kt
+++ presentation/src/main/java/com/plub/presentation/ui/home/adapter/viewholder/MainRecommendListViewHolder.kt
@@ -2,8 +2,10 @@ package com.plub.presentation.ui.home.adapter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
 import com.plub.domain.model.vo.home.recommendationgatheringvo.RecommendationGatheringResponseContentListVo
+import com.plub.presentation.R
 import com.plub.presentation.databinding.IncludeItemRecommendGatheringListItemBinding
 import com.plub.presentation.ui.home.adapter.MainRecommendGatheringAdapter
+import com.plub.presentation.util.GlideUtil
 
 class MainRecommendListViewHolder(
     private val binding: IncludeItemRecommendGatheringListItemBinding,
@@ -16,7 +18,15 @@ class MainRecommendListViewHolder(
                 listener.onClick(item.plubbingId)
             }
 
-
+            GlideUtil.loadImage(root.context, item.mainImage, imageViewMeet)
+            if(item.isBookmarked){
+                imageBtnBookmark.setImageResource(R.drawable.ic_bookmark_checked)
+            }
+            textViewMeetTitle.text = item.title
+            textViewMeetOnelineIntroduce.text = item.introduce
+            //textViewLocation.text =
+            //textViewPeople.text = "모집 인원 ${item.maxAccountNum}"
+            //textViewDate.text = "매주 ~
         }
     }
 }
