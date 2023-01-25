944d94ae335667890c86ccb04304d07caa32a782
hammond <kgbduswns@gmail.com>
2023-01-25 17:54:02 +0900

    feat-137 : PlubCard List,Grid ViewHolder 북마크 클릭 리스너 추가 및 모임장소, 모임날짜 bind 추가

--- presentation/src/main/java/com/plub/presentation/ui/home/adapter/PlubCardGridViewHolder.kt
+++ presentation/src/main/java/com/plub/presentation/ui/home/adapter/PlubCardGridViewHolder.kt
@@ -4,7 +4,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.plub.domain.model.vo.plub.PlubCardVo
 import com.plub.presentation.R
 import com.plub.presentation.databinding.IncludeItemPlubCardGridBinding
-import com.plub.presentation.util.GlideUtil
+import com.plub.presentation.util.TimeFormatter
 
 class PlubCardGridViewHolder(
     private val binding: IncludeItemPlubCardGridBinding,
@@ -14,6 +14,12 @@ class PlubCardGridViewHolder(
     private var vo: PlubCardVo? = null
 
     init {
+        binding.imageViewBookmark.setOnClickListener {
+            vo?.let {
+                listener.onClickBookmark(it.id)
+            }
+        }
+
         binding.root.setOnClickListener {
             vo?.let {
                 listener.onClickPlubCard(it.id)
@@ -24,9 +30,11 @@ class PlubCardGridViewHolder(
     fun bind(item: PlubCardVo) {
         vo = item
         binding.apply {
+            val time = TimeFormatter.getAmPmHourMin(item.time)
+            val memberCount = root.context.getString(R.string.plub_recruit_count, item.remainMemberNumber)
             textViewLocation.text = item.place
-            textViewMeetingDate.text = item.place
-            textViewRecruitMemberCount.text = item.remainMemberNumber.toString()
+            textViewMeetingDate.text = time
+            textViewRecruitMemberCount.text = memberCount
             textViewName.text = item.title
             val bookmarkRes =
                 if (item.isBookmarked) R.drawable.ic_bookmark_checked else R.drawable.ic_bookmark_unchecked
--- presentation/src/main/java/com/plub/presentation/ui/home/adapter/PlubCardListViewHolder.kt
+++ presentation/src/main/java/com/plub/presentation/ui/home/adapter/PlubCardListViewHolder.kt
@@ -4,16 +4,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.plub.domain.model.vo.plub.PlubCardVo
 import com.plub.presentation.R
 import com.plub.presentation.databinding.IncludeItemPlubCardListBinding
-import com.plub.presentation.util.GlideUtil
+import com.plub.presentation.util.TimeFormatter
 
 class PlubCardListViewHolder(
     private val binding: IncludeItemPlubCardListBinding,
     private val listener: PlubCardAdapter.Delegate
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    companion object {
+        private const val SEPARATOR_OF_DAY = ","
+    }
+
     private var vo: PlubCardVo? = null
 
     init {
+        binding.imageViewBookmark.setOnClickListener {
+            vo?.let {
+                listener.onClickBookmark(it.id)
+            }
+        }
+
         binding.root.setOnClickListener {
             vo?.let {
                 listener.onClickPlubCard(it.id)
@@ -24,9 +34,13 @@ class PlubCardListViewHolder(
     fun bind(item: PlubCardVo) {
         vo = item
         binding.apply {
+            val days = item.days.joinToString(SEPARATOR_OF_DAY)
+            val time = TimeFormatter.getAmPmHourMin(item.time)
+            val date = root.context.getString(R.string.word_middle_line, days, time)
+            val memberCount = root.context.getString(R.string.plub_recruit_count, item.remainMemberNumber)
             textViewLocation.text = item.place
-            textViewMeetingDate.text = item.place
-            textViewRecruitMemberCount.text = item.remainMemberNumber.toString()
+            textViewMeetingDate.text = date
+            textViewRecruitMemberCount.text = memberCount
             textViewTitle.text = item.title
             textViewName.text = item.name
             val bookmarkRes =
