1ab8794b3e732424f587155d6d705bd421fde6e3
koownij <koownij@kakao.com>
2023-02-06 13:11:57 +0900

    fix-142: EventFlow 캐싱하는 경우, 두 개 이상의 subscriber에게 emit되지 않는 현상 수정

--- presentation/src/main/java/com/plub/presentation/util/EventFlow.kt
+++ presentation/src/main/java/com/plub/presentation/util/EventFlow.kt
@@ -4,6 +4,9 @@ import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 import java.util.concurrent.atomic.AtomicBoolean
 
 interface EventFlow<out T> : Flow<T> {
@@ -31,11 +34,47 @@ private class EventFlowImpl<T>(
 
     private val flow: MutableSharedFlow<EventFlowSlot<T>> = MutableSharedFlow(replay = replay)
 
+    // first : prevSlot second : currentSlot
+    private val slotStore: HashMap<String, EventFlowSlot<T>> = hashMapOf()
+
+
+
     @InternalCoroutinesApi
     override suspend fun collect(collector: FlowCollector<T>) = flow
         .collect { slot ->
-            if (!slot.markConsumed()) {
-                collector.emit(slot.value)
+
+            /**
+             * A, B, C Fragment에서 D CacheEventflow를 subscribe 하고 있다고 가정해보자.
+             * D CacheEventflow가 emit되면 A, B, C Fragment에서 전부 같은 Event 객체가 수집된다. (Eventflow 객체의 주소는 @f12622로 전부 같다.)
+             * CacheEventflow의 경우 한 번만 소비 되기 때문에
+             * A Fragment에서 D CacheEventflow(@f12622)가 소비되면
+             * B, C collector에서 D CacheEventflow(@f12622)를 소비할 수 없다.
+             */
+
+            /**
+             * 따라서 이를 해결하기 위해 slotStore를 만들었음.
+             * (slot == CacheEventflow)
+             * key에는 collect가 생성된 디렉토리 위치 와 slot의 주소 값을 종합해서 만들었음.
+             * value에는 해당 slot의 value 값을 가지는 새로운 EventFlowSlot를 만들어서 넣어줌
+             *
+             * 이렇게 하면 A, B, C Fragment에서 D CacheEventflow를 subscribe 하고 있을 때
+             * D CacheEventflow가 emit되면 slotStore에 각각 다른 주소를 가진 D-A, D-B, D-C CacheEventflow가 저장된다.
+             * 따라서 A Fragment에서는 D-A CacheEventflow가, B Fragment에서는 D-B CacheEventflow가 ... 소비되므로 안전하다.
+             * 또한 D-A, D-B, D-C CacheEventflow는 각각 한번 씩만 호출되므로, 같은 Fragment에서 동일한 CacheEventflow emit되더라도 한번만 소비된다.
+             */
+            PlubLogger.logD("EventFlow","현 collector 주소 ${collector.hashCode()}, 수집된 값 : ${slot}")
+
+            val slotKey = collector.javaClass.name + slot
+            PlubLogger.logD("EventFlow","slotKey $slotKey")
+            if(slotStore.containsKey(slotKey).not()) {
+                PlubLogger.logD("EventFlow","slotStore 새로 생성 ${slotStore}")
+                slotStore[slotKey] = EventFlowSlot(slot.value)
+            }
+
+            val slotValue = slotStore[slotKey] ?: slot
+
+            if (!slotValue.markConsumed()) {
+                collector.emit(slotValue.value)
             }
         }
 
