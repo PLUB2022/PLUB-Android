a7f98e141078dba0d55d5bccad63c50089433ae0
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-02-12 17:58:12 +0900

    feat-148 : 아카이브 상세 뷰페이저

--- presentation/src/main/java/com/plub/presentation/ui/main/archive/ArchiveFragment.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/archive/ArchiveFragment.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.plub.domain.model.vo.archive.ArchiveContentResponseVo
+import com.plub.domain.model.vo.archive.ArchiveDetailResponseVo
 import com.plub.presentation.base.BaseFragment
 import com.plub.presentation.databinding.FragmentArchiveBinding
 import com.plub.presentation.ui.PageState
@@ -26,7 +27,10 @@ class ArchiveFragment : BaseFragment<FragmentArchiveBinding, ArchivePageState, A
     private val archiveAdapter: ArchiveAdapter by lazy {
         ArchiveAdapter(object : ArchiveAdapter.ArchiveDelegate {
             override fun onCardClick(archiveId: Int) {
-                viewModel.seeDetailDialog(archiveId)
+                //viewModel.seeDetailDialog(archiveId)
+                val list = arrayListOf<String>("https://plub.s3.ap-northeast-2.amazonaws.com/plubbing/mainImage/sports1.png" , "https://plub.s3.ap-northeast-2.amazonaws.com/plubbing/mainImage/sports1.png", "https://plub.s3.ap-northeast-2.amazonaws.com/plubbing/mainImage/2625879414%40KAKAO_873db2c5-613a-41ea-84bd-dec8d194d629_.jpeg")
+                val response = ArchiveDetailResponseVo(list, archiveId,"2023-02-12", "테스트 아카이브")
+                ArchiveDetailDialog(response).show(childFragmentManager, ARCHIVE_DETAIL_DIALOG_TAG)
             }
 
         })
--- presentation/src/main/java/com/plub/presentation/ui/main/archive/dialog/ArchiveDetailDialog.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/archive/dialog/ArchiveDetailDialog.kt
@@ -1,22 +1,25 @@
 package com.plub.presentation.ui.main.archive.dialog
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.RecyclerView
 import com.plub.domain.model.vo.archive.ArchiveDetailResponseVo
 import com.plub.presentation.R
 import com.plub.presentation.databinding.IncludeDialogDetailArchiveBinding
+import com.plub.presentation.util.px
 
 class ArchiveDetailDialog(
-    private val detailVo : ArchiveDetailResponseVo
-) : DialogFragment(){
-    private val binding : IncludeDialogDetailArchiveBinding by lazy {
+    private val detailVo: ArchiveDetailResponseVo
+) : DialogFragment() {
+    private val binding: IncludeDialogDetailArchiveBinding by lazy {
         IncludeDialogDetailArchiveBinding.inflate(layoutInflater)
     }
 
-    private val archiveViewPagerAdapter : ArchiveViewPagerAdapter by lazy {
+    private val archiveViewPagerAdapter: ArchiveViewPagerAdapter by lazy {
         ArchiveViewPagerAdapter()
     }
 
@@ -25,7 +28,6 @@ class ArchiveDetailDialog(
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dialog?.setCancelable(false)
         return binding.root
     }
 
@@ -35,15 +37,34 @@ class ArchiveDetailDialog(
     }
 
 
-    private fun initView(){
+    private fun initView() {
         binding.apply {
+            initRecycler()
             archiveViewPagerAdapter.submitList(detailVo.images)
-            viewPagerArchiveImage.apply {
-                adapter = archiveViewPagerAdapter
-            }
             textViewSequence.text = getString(R.string.archive_dialog_sequence, detailVo.sequence)
             textViewCreateDate.text = detailVo.createdAt
             textViewTitle.text = detailVo.title
+        }
+    }
+
+    private fun initRecycler(){
+        binding.viewPagerArchiveImage.apply {
+            addItemDecoration(object : RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    outRect.right = 16.px
+                    outRect.left = 16.px
+                }
+            })
+            setPageTransformer { page, position ->
+                page.translationX = -(24.px) * (position)
+            }
+            offscreenPageLimit = 1
+            adapter = archiveViewPagerAdapter
         }
     }
 }
