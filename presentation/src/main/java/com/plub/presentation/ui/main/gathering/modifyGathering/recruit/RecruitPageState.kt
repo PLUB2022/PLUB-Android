97fae1264f3b955443f9c9f9f67edc1cf812cacc
koownij <koownij@kakao.com>
2023-02-17 18:05:23 +0900

    feat-153: parcelize 기능 추가 및 RecruitPageState Parcelable 추가

--- presentation/src/main/java/com/plub/presentation/ui/main/gathering/modifyGathering/recruit/RecruitPageState.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/gathering/modifyGathering/recruit/RecruitPageState.kt
@@ -1,6 +1,7 @@
 package com.plub.presentation.ui.main.gathering.modifyGathering.recruit
 
 import android.graphics.Bitmap
+import android.os.Parcelable
 import com.plub.presentation.ui.PageState
 import com.plub.presentation.ui.main.gathering.createGathering.dayAndOnOfflineAndLocation.CreateGatheringDayAndTimeAndOnOfflineAndLocationPageState
 import com.plub.presentation.ui.main.gathering.createGathering.gatheringTitleAndName.CreateGatheringTitleAndNamePageState
@@ -8,7 +9,9 @@ import com.plub.presentation.ui.main.gathering.createGathering.goalAndIntroduceA
 import com.plub.presentation.ui.main.gathering.createGathering.peopleNumber.CreateGatheringPeopleNumberPageState
 import com.plub.presentation.ui.main.gathering.createGathering.question.CreateGatheringQuestionPageState
 import com.plub.presentation.ui.main.gathering.createGathering.selectPlubCategory.CreateGatheringSelectPlubCategoryPageState
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class RecruitPageState(
     val title: String = "",
     val name: String = "",
@@ -16,4 +19,4 @@ data class RecruitPageState(
     val introduce: String = "",
     val plubbingMainImgUrl: String = "",
     val tempPlubbingMainBitmap: Bitmap? = null
-) : PageState
+) : PageState, Parcelable
