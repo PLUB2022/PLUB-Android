31f187717b300658df7edc798eaec8591b74ddc3
koownij <koownij@kakao.com>
2023-02-21 11:56:20 +0900

    feat-164: ModifyGuestQuestionEvent 세팅

--- presentation/src/main/java/com/plub/presentation/ui/main/gathering/modifyGathering/guestQuestion/ModifyGuestQuestionEvent.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/gathering/modifyGathering/guestQuestion/ModifyGuestQuestionEvent.kt
@@ -3,5 +3,6 @@ package com.plub.presentation.ui.main.gathering.modifyGathering.guestQuestion
 import com.plub.presentation.ui.Event
 
 sealed class ModifyGuestQuestionEvent : Event {
-
+    data class ShowBottomSheetDeleteQuestion(val size: Int, val position: Int) : ModifyGuestQuestionEvent()
+    object PerformClickNoQuestionRadioButton: ModifyGuestQuestionEvent()
 }
