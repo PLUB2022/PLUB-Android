25a736d14acb8f38011648c4fdbd273d24463c31
koownij <koownij@kakao.com>
2023-02-21 15:28:04 +0900

    chore-164: Code Clean up

--- presentation/src/main/java/com/plub/presentation/ui/main/gathering/createGathering/question/CreateGatheringQuestionFragment.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/gathering/createGathering/question/CreateGatheringQuestionFragment.kt
@@ -60,7 +60,7 @@ class CreateGatheringQuestionFragment : BaseFragment<
                     if (viewLifecycleOwner.lifecycle.currentState != Lifecycle.State.RESUMED) return@collect
 
                     when (it) {
-                        is com.plub.presentation.ui.main.gathering.createGathering.CreateGatheringEvent.GoToPrevPage -> {
+                        is CreateGatheringEvent.GoToPrevPage -> {
                             parentViewModel.setChildrenPageState(viewModel.uiState.value)
                             parentViewModel.goToPrevPageAndEmitChildrenPageState()
                         }
