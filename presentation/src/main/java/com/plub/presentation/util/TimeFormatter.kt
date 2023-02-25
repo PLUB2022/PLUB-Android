c8e382a368065ab3f3ada13f087eadc704447fa2
koownij <koownij@kakao.com>
2023-02-26 00:42:10 +0900

    feat-165: a h:mm, yyyy. MM. dd E 형식 TimeFormatter 함수 추가

--- presentation/src/main/java/com/plub/presentation/util/TimeFormatter.kt
+++ presentation/src/main/java/com/plub/presentation/util/TimeFormatter.kt
@@ -1,5 +1,6 @@
 package com.plub.presentation.util
 
+import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.LocalTime
 
@@ -8,10 +9,14 @@ object TimeFormatter {
     private const val INDEX_OF_HOUR = 0
     private const val INDEX_OF_MIN = 1
     private const val ahmLocaleKorean = "a h시 m분"
+    private const val ah_colon_mm = "a h:mm"
     private const val HHmm = "HHmm"
+    private const val yyyydotMMdotddE = "yyyy. MM. dd E"
 
     private val ahmLocaleKoreanFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern(ahmLocaleKorean)
+    private val ah_colon_mmFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern(ah_colon_mm)
     private val HHmmFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern(HHmm)
+    private val yyyydotMMdotddEFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern(yyyydotMMdotddE)
 
     fun getAmPmHourMin(time:String): String {
         val list = time.split(SPLIT_OF_TIME)
@@ -23,7 +28,15 @@ object TimeFormatter {
         return ahmLocaleKoreanFormatter.format(LocalTime.of(hour, min))
     }
 
+    fun getAmPmHourColonMin(hour: Int, min: Int): String {
+        return ah_colon_mmFormatter.format(LocalTime.of(hour, min))
+    }
+
     fun getHHmm(hour: Int, min: Int): String {
         return HHmmFormatter.format(LocalTime.of(hour, min))
+    }
+
+    fun getyyyydotMMdotddE(year: Int, month: Int, day: Int): String {
+        return yyyydotMMdotddEFormatter.format(LocalDate.of(year, month, day))
     }
 }
