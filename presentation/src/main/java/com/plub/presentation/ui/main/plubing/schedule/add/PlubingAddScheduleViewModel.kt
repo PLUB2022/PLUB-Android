73e030b64b89c486dc7878a9d894a644c54d4839
koownij <koownij@kakao.com>
2023-02-25 20:25:57 +0900

    feat-165: 일정 추가 - 일정 제목, 하루 종일, 메모 데이터바인딩

--- presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/add/PlubingAddScheduleViewModel.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/add/PlubingAddScheduleViewModel.kt
@@ -1,5 +1,6 @@
 package com.plub.presentation.ui.main.plubing.schedule.add
 
+import android.text.Editable
 import com.plub.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,5 +10,21 @@ class PlubingAddScheduleViewModel @Inject constructor(
 
 ) : BaseViewModel<PlubingAddSchedulePageState>(PlubingAddSchedulePageState()) {
 
+    fun updateScheduleTitle(text: Editable) {
+        updateUiState { uiState ->
+            uiState.copy(scheduleTitle = text.toString())
+        }
+    }
 
+    fun updateMemo(text: Editable) {
+        updateUiState { uiState ->
+            uiState.copy(memo = text.toString())
+        }
+    }
+
+    fun onClickIsAllDay() {
+        updateUiState { uiState ->
+            uiState.copy(isAllDay = uiState.isAllDay.not())
+        }
+    }
 }
