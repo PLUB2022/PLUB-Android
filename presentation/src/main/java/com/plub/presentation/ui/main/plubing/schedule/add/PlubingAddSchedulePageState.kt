c1b80d6710057d80aeb18b4ad1a7c07023b1a1de
koownij <koownij@kakao.com>
2023-02-26 00:42:49 +0900

    feat-165: 일정 추가 -> 날짜/시간 PageState 적용 및 DataBinding 적용

--- presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/add/PlubingAddSchedulePageState.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/add/PlubingAddSchedulePageState.kt
@@ -1,17 +1,32 @@
 package com.plub.presentation.ui.main.plubing.schedule.add
 
-import com.plub.domain.model.enums.PlubingMainPageType
-import com.plub.domain.model.vo.plub.PlubingMemberInfoVo
 import com.plub.presentation.ui.PageState
+import com.plub.presentation.util.TimeFormatter
+import java.util.Calendar
 
 data class PlubingAddSchedulePageState(
     val scheduleTitle: String = "",
     val isAllDay: Boolean = false,
-    val month: Int = 1,
-    val day: Int = 1,
-    val hour: Int = 1,
-    val minute: Int = 1,
+    val startDate: Date = Date(),
+    val startTime: Time = Time(),
+    val endDate: Date = Date(),
+    val endTime: Time = Time(),
     val location: String = "",
     val alarm: String = "",
     val memo: String = ""
 ) : PageState
+
+data class Date(
+    val year: Int = Calendar.getInstance().get(Calendar.YEAR),
+    val month: Int = Calendar.getInstance().get(Calendar.MONTH) + 1,
+    val day: Int = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+) {
+    val text = TimeFormatter.getyyyydotMMdotddE(year, month, day)
+}
+
+data class Time(
+    val hour: Int = Calendar.getInstance().get(Calendar.HOUR),
+    val minute: Int = Calendar.getInstance().get(Calendar.MINUTE)
+) {
+    val text = TimeFormatter.getAmPmHourColonMin(hour, minute)
+}
