bbb7bef4a768fcd354254e827a6e657188be0207
koownij <koownij@kakao.com>
2023-03-02 15:58:49 +0900

    feat-161: 로그인 시 FcmToken을 함께 보내도록 수정

--- domain/src/main/java/com/plub/domain/model/vo/login/SocialLoginRequestVo.kt
+++ domain/src/main/java/com/plub/domain/model/vo/login/SocialLoginRequestVo.kt
@@ -6,5 +6,6 @@ import com.plub.domain.model.enums.SocialLoginType
 data class SocialLoginRequestVo(
     val socialLoginType: SocialLoginType,
     val authCode:String = "",
-    val accessToken:String = ""
+    val accessToken:String = "",
+    val fcmToken: String = ""
 ): DomainModel
