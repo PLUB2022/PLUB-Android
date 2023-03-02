e90b5e99c8d275a74c0da66ecbe799476beb9ca1
koownij <koownij@kakao.com>
2023-03-02 15:55:37 +0900

    feat-161: PlubFirebaseMessagingService - 메세지 수신 후 푸시 알림 기능 임시 구현

--- presentation/src/main/java/com/plub/presentation/service/PlubFirebaseMessagingService.kt
+++ presentation/src/main/java/com/plub/presentation/service/PlubFirebaseMessagingService.kt
@@ -2,36 +2,68 @@ package com.plub.presentation.service
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.media.RingtoneManager
 import android.os.Build
-import android.util.Log
 import androidx.core.app.NotificationCompat
-import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.plub.presentation.ui.main.MainActivity
+import com.plub.presentation.R
+import com.plub.presentation.util.IntentUtil
 
 class PlubFirebaseMessagingService : FirebaseMessagingService() {
 
+    companion object {
+
+        const val CHANNEL_ID = "notification_remote_channel"
+    }
+
+    private lateinit var notificationManager: NotificationManager
+
     override fun onNewToken(token: String) {
-
+        super.onNewToken(token)
     }
 
-    /** 메시지 수신 메서드(포그라운드) */
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
 
-        // Notification 메시지를 수신할 경우
-        // remoteMessage.notification?.body!! 여기에 내용이 저장되있음
-        // Log.d(TAG, "Notification Message Body: " + remoteMessage.notification?.body!!)
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
 
-        //받은 remoteMessage의 값 출력해보기. 데이터메세지 / 알림메세지
+        //title, body
+
+        createNotificationChannel()
+        sendNotification(message.data["title"] ?: "", message.data["body"] ?: "")
     }
 
-    /** 알림 생성 메서드 */
-    private fun sendNotification(remoteMessage: RemoteMessage) {
+    private fun createNotificationChannel() {
+        val importance = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) { NotificationManager.IMPORTANCE_HIGH }
+        else { NotificationCompat.PRIORITY_HIGH }
 
+        val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel(CHANNEL_ID, this.getString(R.string.notification_channel_name), importance)
+        } else return
+
+        notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun sendNotification(title: String, body: String) {
+        val pendingIntent = IntentUtil.getFcmPendingIntent(this)
+
+        val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationCompat.Builder(this, CHANNEL_ID)
+        } else NotificationCompat.Builder(this)
+
+        builder
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setFullScreenIntent(pendingIntent, true)
+            .setAutoCancel(true)
+
+        notificationManager.notify(System.currentTimeMillis().toInt(), builder.build())
     }
 }
