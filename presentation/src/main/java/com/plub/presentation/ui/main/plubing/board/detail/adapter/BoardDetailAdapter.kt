59fc45b930d4b4ee5a76851ae736243c51662f43
hammond <kgbduswns@gmail.com>
2023-03-04 10:06:14 +0900

    feat-145: 게시판 상세 adapter 추가 및 코멘트, 상세정보 viewholder 추가

---
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/board/detail/adapter/BoardDetailAdapter.kt
@@ -0,0 +1,77 @@
+package com.plub.presentation.ui.main.plubing.board.detail.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.plub.domain.model.enums.PlubingBoardDetailType
+import com.plub.domain.model.enums.PlubingCommentType
+import com.plub.domain.model.vo.board.BoardCommentVo
+import com.plub.domain.model.vo.board.PlubingBoardVo
+import com.plub.presentation.databinding.IncludeItemBoardDetailCommentNormalBinding
+import com.plub.presentation.databinding.IncludeItemBoardDetailCommentReplyBinding
+import com.plub.presentation.databinding.IncludeItemBoardDetailInfoBinding
+
+class BoardDetailAdapter(
+    val listener: Delegate
+) : ListAdapter<BoardCommentVo, RecyclerView.ViewHolder>(BoardDetailDiffCallback()) {
+
+    companion object {
+        private const val BOARD_DETAIL_INFO_POSITION = 0
+    }
+
+    interface Delegate {
+        fun onClickBoardMenu(vo: PlubingBoardVo)
+        fun onClickCommentMenu(vo: BoardCommentVo)
+        fun onClickCommentReply(vo: BoardCommentVo)
+        val boardVo: PlubingBoardVo
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is BoardDetailInfoViewHolder -> holder.bind(listener.boardVo)
+            is BoardDetailCommentNormalViewHolder -> holder.bind(currentList[position])
+            is BoardDetailCommentReplyViewHolder -> holder.bind(currentList[position])
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (PlubingBoardDetailType.indexOf(viewType)) {
+            PlubingBoardDetailType.DETAIL -> {
+                val binding = IncludeItemBoardDetailInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                BoardDetailInfoViewHolder(binding, listener)
+            }
+
+            PlubingBoardDetailType.COMMENT -> {
+                val binding = IncludeItemBoardDetailCommentNormalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                BoardDetailCommentNormalViewHolder(binding, listener)
+            }
+
+            PlubingBoardDetailType.COMMENT_REPLY -> {
+                val binding = IncludeItemBoardDetailCommentReplyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                BoardDetailCommentReplyViewHolder(binding, listener)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            position == BOARD_DETAIL_INFO_POSITION -> PlubingBoardDetailType.DETAIL.idx
+            currentList[position].commentType == PlubingCommentType.COMMENT -> PlubingBoardDetailType.COMMENT.idx
+            else -> PlubingBoardDetailType.COMMENT_REPLY.idx
+        }
+    }
+
+    fun notifyBoardDetail() {
+        notifyItemChanged(BOARD_DETAIL_INFO_POSITION)
+    }
+}
+
+class BoardDetailDiffCallback : DiffUtil.ItemCallback<BoardCommentVo>() {
+    override fun areItemsTheSame(oldItem: BoardCommentVo, newItem: BoardCommentVo): Boolean =
+        oldItem.commentId == newItem.commentId
+
+    override fun areContentsTheSame(oldItem: BoardCommentVo, newItem: BoardCommentVo): Boolean =
+        oldItem == newItem
+}
---
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/board/detail/adapter/BoardDetailCommentReplyViewHolder.kt
@@ -0,0 +1,42 @@
+package com.plub.presentation.ui.main.plubing.board.detail.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.plub.domain.model.vo.board.BoardCommentVo
+import com.plub.presentation.R
+import com.plub.presentation.databinding.IncludeItemBoardDetailCommentReplyBinding
+import com.plub.presentation.util.GlideUtil
+
+class BoardDetailCommentReplyViewHolder(
+    private val binding: IncludeItemBoardDetailCommentReplyBinding,
+    private val listener: BoardDetailAdapter.Delegate
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private var vo: BoardCommentVo? = null
+
+    init {
+        binding.imageViewMenu.setOnClickListener {
+            vo?.let {
+                listener.onClickCommentMenu(it)
+            }
+        }
+
+        binding.textViewReply.setOnClickListener {
+            vo?.let {
+                listener.onClickCommentReply(it)
+            }
+        }
+    }
+
+    fun bind(item: BoardCommentVo) {
+        vo = item
+        binding.apply {
+            textViewMarkAuthor.visibility = if(item.isFeedAuthor) View.VISIBLE else View.GONE
+            textViewDate.text = item.createAt
+            textViewNickname.text = item.nickname
+            textViewContent.text = item.content
+            textViewReplyParent.text = root.context.getString(R.string.plubing_board_detail_replied, item.parentCommentNickname)
+            GlideUtil.loadImage(root.context, item.profileImage, circleImageViewProfile)
+        }
+    }
+}
