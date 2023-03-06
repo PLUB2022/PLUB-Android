5497ae41f3acb6ec5ccc0e490406b56c13fdd5c7
koownij <koownij@kakao.com>
2023-03-06 16:44:09 +0900

    feat-165: GetEntireScheduleUseCase 추가

---
+++ domain/src/main/java/com/plub/domain/usecase/GetEntireScheduleUseCase.kt
@@ -0,0 +1,20 @@
+package com.plub.domain.usecase
+
+import com.plub.domain.UiState
+import com.plub.domain.base.UseCase
+import com.plub.domain.model.vo.board.FetchPlubingBoardRequestVo
+import com.plub.domain.model.vo.board.PlubingBoardListVo
+import com.plub.domain.model.vo.schedule.GetEntireScheduleRequestVo
+import com.plub.domain.model.vo.schedule.GetEntireScheduleResponseVo
+import com.plub.domain.repository.PlubingBoardRepository
+import com.plub.domain.repository.ScheduleRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetEntireScheduleUseCase @Inject constructor(
+    private val scheduleRepository: ScheduleRepository
+) : UseCase<GetEntireScheduleRequestVo, Flow<UiState<GetEntireScheduleResponseVo>>>() {
+    override suspend operator fun invoke(request: GetEntireScheduleRequestVo): Flow<UiState<GetEntireScheduleResponseVo>> {
+        return scheduleRepository.getEntireSchedule(request)
+    }
+}
