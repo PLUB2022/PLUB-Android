e32ddaf1bec6a7b942c2ed383f314ca9be0935c4
koownij <koownij@kakao.com>
2023-03-08 14:43:50 +0900

    feat-165: PlubingSchedulePageState 세팅

--- presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/PlubingSchedulePageState.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/PlubingSchedulePageState.kt
@@ -1,9 +1,13 @@
 package com.plub.presentation.ui.main.plubing.schedule
 
 import com.plub.domain.model.enums.PlubingMainPageType
+import com.plub.domain.model.vo.board.PlubingBoardVo
 import com.plub.domain.model.vo.plub.PlubingMemberInfoVo
+import com.plub.domain.model.vo.schedule.ScheduleVo
 import com.plub.presentation.ui.PageState
 
 data class PlubingSchedulePageState(
-    val plubbingId: Int = -1
+    val plubbingId: Int = -1,
+    val scheduleList:List<ScheduleVo> = emptyList(),
+    val scheduleCursorId: Int = 0
 ) : PageState
