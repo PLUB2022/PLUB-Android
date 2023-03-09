349db567c6a449dd09594f3bccd4b7d0520eaefe
koownij <koownij@kakao.com>
2023-03-09 18:01:51 +0900

    feat-165: bottom sheet 접힌 상태의 프로필 RecyclerView 설정

--- presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/bottomSheet/BottomSheetScheduleDetail.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/bottomSheet/BottomSheetScheduleDetail.kt
@@ -1,29 +1,48 @@
 package com.plub.presentation.ui.main.plubing.schedule.bottomSheet
 
+import android.app.Activity
+import android.graphics.Point
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.plub.domain.model.vo.schedule.ScheduleVo
 import com.plub.presentation.R
-import com.plub.presentation.databinding.BottomSheetDeleteQuestionBinding
 import com.plub.presentation.databinding.BottomSheetScheduleDetailBinding
+import com.plub.presentation.ui.common.decoration.GridSpaceDecoration
+import com.plub.presentation.ui.sign.hobbies.adapter.HobbyViewHolder
 import com.plub.presentation.util.TimeFormatter
+import com.plub.presentation.util.px
 import com.plub.presentation.util.serializable
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class BottomSheetScheduleDetail : BottomSheetDialogFragment() {
+
     private val binding: BottomSheetScheduleDetailBinding by lazy {
         BottomSheetScheduleDetailBinding.inflate(layoutInflater)
     }
 
+    private val maxProfile: Int by lazy { (getScreenWidth() - PADDING_HORIZONTAL.px) / PROFILE_WIDTH.px }
+
+    private val foldProfileAdapter: FoldProfileAdapter by lazy {
+        FoldProfileAdapter(maxProfile)
+    }
+
     companion object {
         private const val SCHEDULE_VO = "SCHEDULE_VO"
+        private const val PROFILE_WIDTH = 48
+        private const val PADDING_HORIZONTAL = 32
+        private const val ITEM_SPAN_SIZE = 1
+        private const val ITEM_SPACE = 4
+
 
         fun newInstance(
             scheduleVo: ScheduleVo,
@@ -56,17 +75,32 @@ class BottomSheetScheduleDetail : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
-
             textViewTitle.text = scheduleVo.title
             setTextViewMonth(textViewMonth, scheduleVo)
             textViewDate.text = getTextViewDate(scheduleVo)
             textViewTime.text = getTextViewTime(scheduleVo)
             setLocation(textViewLocation, imageViewLocation, scheduleVo)
 
-//            val profileList =
-//                scheduleVo.calendarAttendList.calendarAttendList.map { it.profileImage }
-//            plubingScheduleProfileAdapter.submitList(profileList)
+            recyclerViewAttendFold.apply {
+                layoutManager = GridLayoutManager(context, maxProfile).apply {
+                    spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                        override fun getSpanSize(position: Int): Int {
+                            return ITEM_SPAN_SIZE
+                        }
+                    }
+                }
+                addItemDecoration(GridSpaceDecoration(maxProfile, ITEM_SPACE.px, ITEM_SPACE.px, false))
+                adapter = foldProfileAdapter
+            }
 
+//            recyclerViewAttendFold.apply {
+//                layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+//                adapter = foldProfileAdapter
+//            }
+
+            val profileList =
+                scheduleVo.calendarAttendList.calendarAttendList.map { it.profileImage }
+            foldProfileAdapter.submitList(profileList)
 
             buttonYes.setOnClickListener {
                 okButtonClickEvent?.let { it(scheduleVo.calendarId) }
@@ -101,5 +135,10 @@ class BottomSheetScheduleDetail : BottomSheetDialogFragment() {
             textViewLocation.visibility = View.GONE
             imageViewLocation.visibility = View.GONE
         } else textViewLocation.text = item.placeName
+    }
+
+    private fun getScreenWidth(): Int {
+        val display = requireActivity().applicationContext?.resources?.displayMetrics
+        return display?.widthPixels ?: 0
     }
 }
---
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/schedule/bottomSheet/FoldProfileViewHolder.kt
@@ -0,0 +1,31 @@
+package com.plub.presentation.ui.main.plubing.schedule.bottomSheet
+
+import androidx.recyclerview.widget.RecyclerView
+import com.plub.domain.model.vo.home.recruitdetailvo.RecruitDetailJoinedAccountsVo
+import com.plub.presentation.R
+import com.plub.presentation.databinding.IncludeItemCircleProfileBinding
+import com.plub.presentation.databinding.LayoutRecyclerScheduleDetailFoldProfileBinding
+import com.plub.presentation.util.GlideUtil
+
+class FoldProfileViewHolder(
+    private val binding: LayoutRecyclerScheduleDetailFoldProfileBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+
+    }
+
+    fun bind(item: String, position: Int, allPeopleCount: Int, maxProfile : Int) {
+        val nowNum = position + 1
+        val morePeopleCount = allPeopleCount - position
+        binding.apply {
+            if (nowNum == maxProfile) {
+                textViewMoreProfileNumber.text = root.context.getString(R.string.detail_recruitment_profile_county, morePeopleCount)
+            }
+            else{
+                GlideUtil.loadImage(root.context, item, imageViewProfile)
+                imageViewProfile.clipToOutline = true
+            }
+        }
+    }
+}
