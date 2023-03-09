238263cc6d9489e95ec828b0cf543000876a3237
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-03-09 14:10:26 +0900

    style-168 : MyPage Setting XML

--- presentation/src/main/java/com/plub/presentation/ui/main/home/profile/MyPageFragment.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/home/profile/MyPageFragment.kt
@@ -11,6 +11,8 @@ import com.plub.presentation.base.BaseFragment
 import com.plub.presentation.databinding.FragmentMyPageBinding
 import com.plub.presentation.ui.PageState
 import com.plub.presentation.ui.main.home.profile.adapter.MyPageParentGatheringAdapter
+import com.plub.presentation.util.GlideUtil
+import com.plub.presentation.util.PlubUser
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -43,9 +45,20 @@ class MyPageFragment :
         binding.apply {
             vm = viewModel
             viewModel.getMyPageData()
+            // TODO 주석 제거 및 아래 하드코딩 제거
+            // setMyInfo()
             textViewProfileName.text = "조경석"
             textViewProfileExplain.text = "국회는 법률에 저촉되지 아니하는 범위안에서 의사와 내부규율에 관한 규칙을 제정할 수 있다. 국회는 법률에 저촉되지 아니하는 범위안에서 의사와 내부규율에 관한 규칙을 제정할 수 있다. 국회는 법률에 저촉되지 아니하는 범위안에서 의사와 내부규율에 관한 규칙을 제정할 수 있다."
             initRecycler()
+        }
+    }
+
+    private fun setMyInfo(){
+        binding.apply {
+            textViewProfileName.text = PlubUser.info.nickname
+            textViewProfileExplain.text = PlubUser.info.introduce
+            GlideUtil.loadImage(root.context, PlubUser.info.profileImage, imageViewProfile)
+            imageViewProfile.clipToOutline = true
         }
     }
 
