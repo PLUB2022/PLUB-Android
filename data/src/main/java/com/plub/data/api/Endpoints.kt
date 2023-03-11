2ef8015df35b939db2e0812ca66511a5fbbfc014
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-03-12 01:18:22 +0900

    feat-168 : api추가

--- data/src/main/java/com/plub/data/api/Endpoints.kt
+++ data/src/main/java/com/plub/data/api/Endpoints.kt
@@ -72,6 +72,8 @@ object Endpoints {
 
         object MY_PAGE{
             const val BROWSE_MY_GATHERING = "$PLUBBING_URL/all/my"
+            const val BROWSE_MY_POST = "$PLUBBING_URL/{plubbingId}/feed/my"
+            const val BROWSE_MY_TODO = "$PLUBBING_URL/{plubbingId}/todo/my"
         }
     }
 }
--- data/src/main/java/com/plub/data/api/MyPageApi.kt
+++ data/src/main/java/com/plub/data/api/MyPageApi.kt
@@ -5,11 +5,24 @@ import com.plub.data.dto.myPage.MyGatheringResponse
 import com.plub.data.dto.signUp.NicknameCheckResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MyPageApi {
     @GET(Endpoints.PLUBBING.MY_PAGE.BROWSE_MY_GATHERING)
     suspend fun getMyGathering(
         @Query("status") status: String
+    ): Response<ApiResponse<MyGatheringResponse>>
+
+    @GET(Endpoints.PLUBBING.MY_PAGE.BROWSE_MY_TODO)
+    suspend fun getMyToDo(
+        @Path("plubbingId") plubbingId : Int,
+        @Query("cursorId") cursorId: Int
+    ): Response<ApiResponse<MyGatheringResponse>>
+
+    @GET(Endpoints.PLUBBING.MY_PAGE.BROWSE_MY_POST)
+    suspend fun getMyPost(
+        @Path("plubbingId") plubbingId : Int,
+        @Query("cursorId") cursorId: Int
     ): Response<ApiResponse<MyGatheringResponse>>
 }
