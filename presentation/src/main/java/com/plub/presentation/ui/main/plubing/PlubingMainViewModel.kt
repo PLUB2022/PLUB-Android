969b6bfb54db329b54844955b9d6565d7c4f2210
hammond <kgbduswns@gmail.com>
2023-03-15 03:39:01 +0900

    feat-146: PlubingMain 화면 BaseTestFragment,ViewModel로 변경 및 코드정리

--- presentation/src/main/java/com/plub/presentation/ui/main/plubing/PlubingMainViewModel.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/plubing/PlubingMainViewModel.kt
@@ -3,14 +3,18 @@ package com.plub.presentation.ui.main.plubing
 import androidx.lifecycle.viewModelScope
 import com.plub.domain.model.enums.PlubingMainPageType
 import com.plub.domain.model.vo.plub.PlubingMainVo
+import com.plub.domain.model.vo.plub.PlubingMemberInfoVo
 import com.plub.domain.usecase.FetchPlubingMainUseCase
 import com.plub.presentation.R
-import com.plub.presentation.base.BaseViewModel
+import com.plub.presentation.base.BaseTestViewModel
 import com.plub.presentation.util.PlubingInfo
 import com.plub.presentation.util.ResourceProvider
 import com.plub.presentation.util.TimeFormatter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.math.abs
@@ -20,7 +24,7 @@ import kotlin.properties.Delegates
 class PlubingMainViewModel @Inject constructor(
     val fetchPlubingMainUseCase: FetchPlubingMainUseCase,
     val resourceProvider: ResourceProvider,
-) : BaseViewModel<PlubingMainPageState>(PlubingMainPageState()) {
+) : BaseTestViewModel<PlubingMainPageState>() {
 
     companion object {
         private const val SEPARATOR_OF_DAY = ","
@@ -29,15 +33,29 @@ class PlubingMainViewModel @Inject constructor(
 
     private var plubingId by Delegates.notNull<Int>()
 
+    private val headerAlphaStateFlow: MutableStateFlow<Float> = MutableStateFlow(1f)
+    private val plubingNameStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+    private val plubingDateStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+    private val plubingLocationStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+    private val plubingMainImageStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+    private val pageTypeStateFlow: MutableStateFlow<PlubingMainPageType> = MutableStateFlow(PlubingMainPageType.BOARD)
+    private val memberListStateFlow: MutableStateFlow<List<PlubingMemberInfoVo>> = MutableStateFlow(emptyList())
+
+    override val uiState: PlubingMainPageState = PlubingMainPageState(
+        headerAlphaStateFlow.asStateFlow(),
+        plubingNameStateFlow.asStateFlow(),
+        plubingDateStateFlow.asStateFlow(),
+        plubingLocationStateFlow.asStateFlow(),
+        plubingMainImageStateFlow.asStateFlow(),
+        pageTypeStateFlow.asStateFlow(),
+        memberListStateFlow.asStateFlow(),
+    )
+
     fun onAppBarOffsetChanged(height: Float, totalScrollRange: Int) {
         viewModelScope.launch(Dispatchers.Default) {
             val offsetAlpha: Float = height / totalScrollRange
             val headerAlpha = abs(offsetAlpha)
-            updateUiState {
-                it.copy(
-                    headerAlpha = headerAlpha
-                )
-            }
+            updateHeaderAlpha(headerAlpha)
         }
     }
 
@@ -55,15 +73,11 @@ class PlubingMainViewModel @Inject constructor(
 
     fun onChangedPage(position: Int) {
         val pageType = PlubingMainPageType.valueOf(position)
-        updateUiState { uiState ->
-            uiState.copy(
-                pageType = pageType
-            )
-        }
+        updatePageType(pageType)
     }
 
     fun onClickWrite() {
-        when(uiState.value.pageType) {
+        when(pageTypeStateFlow.value) {
             PlubingMainPageType.BOARD -> emitEventFlow(PlubingMainEvent.GoToWriteBoard)
             PlubingMainPageType.TODO_LIST -> emitEventFlow(PlubingMainEvent.GoToPlannerTodo)
         }
@@ -80,19 +94,54 @@ class PlubingMainViewModel @Inject constructor(
             val time = TimeFormatter.getAmPmHourMin(time)
             val date = getStringResource(R.string.word_middle_line, days, time)
             val location = listOf(address, roadAddress, placeName).joinToString(SEPARATOR_OF_LOCATE)
-            updateUiState {
-                it.copy(
-                    plubingName = name,
-                    plubingDate = date,
-                    plubingLocation = location,
-                    plubingMainImage = mainImage,
-                    memberList = memberInfoList
-                )
-            }
+            updatePlubingName(name)
+            updatePlubingDate(date)
+            updatePlubingLocation(location)
+            updatePlubingMainImage(mainImage)
+            updatePlubingMemberList(memberInfoList)
         }
     }
 
     private fun getStringResource(res: Int, vararg formatArgs: Any?): String {
         return resourceProvider.getString(res, *formatArgs)
+    }
+
+    private fun updateHeaderAlpha(alpha: Float) {
+        viewModelScope.launch {
+            headerAlphaStateFlow.update { alpha }
+        }
+    }
+
+    private fun updatePageType(type: PlubingMainPageType) {
+        viewModelScope.launch {
+            pageTypeStateFlow.update { type }
+        }
+    }
+
+    private fun updatePlubingName(name: String) {
+        viewModelScope.launch {
+            plubingNameStateFlow.update { name }
+        }
+    }
+    private fun updatePlubingDate(date: String) {
+        viewModelScope.launch {
+            plubingDateStateFlow.update { date }
+        }
+    }
+    private fun updatePlubingLocation(location: String) {
+        viewModelScope.launch {
+            plubingLocationStateFlow.update { location }
+        }
+    }
+    private fun updatePlubingMainImage(image: String) {
+        viewModelScope.launch {
+            plubingMainImageStateFlow.update { image }
+        }
+    }
+
+    private fun updatePlubingMemberList(list: List<PlubingMemberInfoVo>) {
+        viewModelScope.launch {
+            memberListStateFlow.update { list }
+        }
     }
 }
