c7cbac8b6413c6f0cd4549f34b2dce7f6edb12df
koownij <koownij@kakao.com>
2023-03-20 16:55:30 +0900

    refactor-171: ScheduleApi 하드 코딩 제거

--- data/src/main/java/com/plub/data/api/ScheduleApi.kt
+++ data/src/main/java/com/plub/data/api/ScheduleApi.kt
@@ -17,18 +17,24 @@ import retrofit2.http.Query
 
 interface ScheduleApi {
 
+    companion object {
+        const val PLUBBING_ID = "plubbingId"
+        const val CALENDAR_ID = "calendarId"
+        const val CURSOR_ID = "cursorId"
+    }
+
     @GET(Endpoints.PLUBBING.SCHEDULE.CALENDAR)
-    suspend fun getEntireSchedule(@Path("plubbingId") plubbingId:Int, @Query("cursorId") cursorId:Int): Response<ApiResponse<GetEntireScheduleResponse>>
+    suspend fun getEntireSchedule(@Path(PLUBBING_ID) plubbingId:Int, @Query(CURSOR_ID) cursorId:Int): Response<ApiResponse<GetEntireScheduleResponse>>
 
     @PUT(Endpoints.PLUBBING.SCHEDULE.ATTEND)
-    suspend fun putScheduleAttend(@Path("plubbingId") plubbingId:Int, @Path("calendarId") calendarId:Int, @Body body: PutScheduleAttendRequest): Response<ApiResponse<CalendarAttendResponse>>
+    suspend fun putScheduleAttend(@Path(PLUBBING_ID) plubbingId:Int, @Path(CALENDAR_ID) calendarId:Int, @Body body: PutScheduleAttendRequest): Response<ApiResponse<CalendarAttendResponse>>
 
     @POST(Endpoints.PLUBBING.SCHEDULE.CALENDAR)
-    suspend fun createSchedule(@Path("plubbingId") plubbingId: Int, @Body body: CreateScheduleRequest): Response<ApiResponse<DataDto.DTO>>
+    suspend fun createSchedule(@Path(PLUBBING_ID) plubbingId: Int, @Body body: CreateScheduleRequest): Response<ApiResponse<DataDto.DTO>>
 
     @PUT(Endpoints.PLUBBING.SCHEDULE.CALENDAR_ID)
-    suspend fun editSchedule(@Path("plubbingId") plubbingId: Int, @Path("calendarId") calendarId:Int, @Body body: CreateScheduleRequest): Response<ApiResponse<DataDto.DTO>>
+    suspend fun editSchedule(@Path(PLUBBING_ID) plubbingId: Int, @Path(CALENDAR_ID) calendarId:Int, @Body body: CreateScheduleRequest): Response<ApiResponse<DataDto.DTO>>
 
     @DELETE(Endpoints.PLUBBING.SCHEDULE.CALENDAR_ID)
-    suspend fun deleteSchedule(@Path("plubbingId") plubbingId:Int, @Path("calendarId") calendarId:Int): Response<ApiResponse<DataDto.DTO>>
+    suspend fun deleteSchedule(@Path(PLUBBING_ID) plubbingId:Int, @Path(CALENDAR_ID) calendarId:Int): Response<ApiResponse<DataDto.DTO>>
 }
