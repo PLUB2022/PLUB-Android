ca544367e94b4c4d1368750a8868839fa255d099
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-03-21 13:50:38 +0900

    delete - 168 : recruitdetail 패키지 제거

--- data/src/main/java/com/plub/data/dto/recruitdetail/host/EndRecruitResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.plub.data.dto.recruitDetail.host
-
-import com.google.gson.annotations.SerializedName
-import com.plub.data.base.DataDto
-
-data class EndRecruitResponse(
-    @SerializedName("plubbingId")
-    val plubbingId : Int = -1
-) : DataDto
