d459d85c56b6b15277f14736d2d853b13addff30
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-03-21 13:49:34 +0900

    delete-168 : applicants recruit 패키지 제거

--- data/src/main/java/com/plub/data/dto/applicantsrecruit/ApplicantsRecruitResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.plub.data.dto.applicantsRecruit
-
-import com.google.gson.annotations.SerializedName
-import com.plub.data.base.DataDto
-
-data class ApplicantsRecruitResponse(
-    @SerializedName("plubbingId")
-    val plubbingId : Int = -1
-) : DataDto
