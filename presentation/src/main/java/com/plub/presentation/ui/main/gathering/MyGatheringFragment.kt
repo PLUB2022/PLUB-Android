c4f6506755665f17e1fd1ceaf71ec80bdb234d65
koownij <koownij@kakao.com>
2023-03-25 20:08:37 +0900

    chore-190: Gathering -> MyGathering 네이밍 변경

--- presentation/src/main/java/com/plub/presentation/ui/main/gathering/MyGatheringFragment.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/gathering/MyGatheringFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [GatheringFragment.newInstance] factory method to
+ * Use the [MyGatheringFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GatheringFragment : Fragment() {
+class MyGatheringFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class GatheringFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gathering, container, false)
+        return inflater.inflate(R.layout.fragment_my_gathering, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class GatheringFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            GatheringFragment().apply {
+            MyGatheringFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
