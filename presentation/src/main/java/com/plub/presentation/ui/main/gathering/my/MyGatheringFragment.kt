8c6f1dc47e0daf9ba7dde259d7a7f201120ba2e2
koownij <koownij@kakao.com>
2023-03-27 18:24:40 +0900

    feat-190: 내 모임 - 리사이클러뷰 임시 연결

--- presentation/src/main/java/com/plub/presentation/ui/main/gathering/my/MyGatheringFragment.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/gathering/my/MyGatheringFragment.kt
@@ -1,18 +1,52 @@
 package com.plub.presentation.ui.main.gathering.my
 
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import com.plub.domain.model.enums.MyGatheringsViewType
+import com.plub.domain.model.vo.myGathering.MyGatheringResponseVo
 import com.plub.presentation.base.BaseTestFragment
 import com.plub.presentation.databinding.FragmentMyGatheringBinding
+import com.plub.presentation.ui.main.gathering.my.adapter.MyGatheringsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MyGatheringFragment : BaseTestFragment<FragmentMyGatheringBinding, MyGatheringPageState, MyGatheringViewModel>(
-    FragmentMyGatheringBinding::inflate
-) {
+class MyGatheringFragment :
+    BaseTestFragment<FragmentMyGatheringBinding, MyGatheringPageState, MyGatheringViewModel>(
+        FragmentMyGatheringBinding::inflate
+    ) {
     override val viewModel: MyGatheringViewModel by viewModels()
+    private val recyclerViewAdapter: MyGatheringsAdapter by lazy {
+        MyGatheringsAdapter(
+            onMyGatheringMeatBallClick = { },
+            onMyHostingMeatBallClick = { },
+            onCreateGatheringClick = { },
+            onParticipateGatheringClick = { }
+        )
+    }
 
     override fun initView() {
+        binding.apply {
+            vm = viewModel
 
+            recyclerView.apply {
+                adapter = recyclerViewAdapter
+                layoutManager = LinearLayoutManager(context).apply {
+                    orientation = LinearLayoutManager.HORIZONTAL
+                }
+
+                val snapHelper = LinearSnapHelper()
+                snapHelper.attachToRecyclerView(this)
+            }
+
+            recyclerViewAdapter.submitList(
+                listOf(
+                    MyGatheringResponseVo(),
+                    MyGatheringResponseVo(),
+                    MyGatheringResponseVo(viewType = MyGatheringsViewType.PARTICIPATE)
+                )
+            )
+        }
     }
 }
