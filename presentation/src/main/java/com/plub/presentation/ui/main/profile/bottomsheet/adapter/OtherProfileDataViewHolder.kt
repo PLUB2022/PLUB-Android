7b72ac20ef012cabe07ff1c64a818e67202ee5ce
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-03-31 13:49:47 +0900

    feat-191 : 임시 어뎁터 뷰홀더 생성

---
+++ presentation/src/main/java/com/plub/presentation/ui/main/profile/bottomsheet/adapter/OtherProfileDataViewHolder.kt
@@ -0,0 +1,21 @@
+package com.plub.presentation.ui.main.profile.bottomsheet.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.plub.domain.model.vo.myPage.MyPageGatheringVo
+import com.plub.presentation.databinding.IncludeItemOtherProfileBinding
+
+class OtherProfileDataViewHolder(
+    private val binding: IncludeItemOtherProfileBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+
+
+
+    init {
+        binding.apply {
+
+        }
+    }
+
+    fun bind(item: MyPageGatheringVo) {
+    }
+}
