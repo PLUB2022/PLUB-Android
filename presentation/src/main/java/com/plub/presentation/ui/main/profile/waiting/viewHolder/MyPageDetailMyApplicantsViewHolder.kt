2d9013369bd138511f77187a92fa33c405f6b2cc
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-04-17 13:18:55 +0900

    feat-168 : 지원 수정 및 기본 이미지 적용

--- presentation/src/main/java/com/plub/presentation/ui/main/profile/waiting/viewHolder/MyPageDetailMyApplicantsViewHolder.kt
+++ presentation/src/main/java/com/plub/presentation/ui/main/profile/waiting/viewHolder/MyPageDetailMyApplicantsViewHolder.kt
@@ -57,7 +57,8 @@ class MyPageDetailMyApplicantsViewHolder(
 
     fun bind(item: AccountsVo) {
         binding.apply {
-            item.profileImage?.let { GlideUtil.loadImage(root.context, it, imageViewProfile) }
+            if(item.profileImage == null || item.profileImage == "") imageViewProfile.setImageResource(R.drawable.iv_default_profile)
+            else GlideUtil.loadImage(root.context, item.profileImage!!, imageViewProfile)
             imageViewProfile.clipToOutline = true
             textViewName.text = item.accountName
             textViewDate.text = root.context.getString(R.string.my_page_complete_answer, item.createdAt)
