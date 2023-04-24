b19ead6313c8b10582ca8f4a01f6976b4483c17c
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-04-24 11:42:15 +0900

    refactor-191 : 빈 화면 제거

--- domain/src/main/java/com/plub/domain/model/enums/HomeViewType.kt
+++ domain/src/main/java/com/plub/domain/model/enums/HomeViewType.kt
@@ -1,7 +1,7 @@
 package com.plub.domain.model.enums
 
 enum class HomeViewType(val type:Int) {
-    TITLE_VIEW(0), CATEGORY_VIEW(1), RECOMMEND_TITLE_VIEW(2), REGISTER_HOBBIES_VIEW(3), RECOMMEND_GATHERING_VIEW(4), EMPTY(5);
+    TITLE_VIEW(0), CATEGORY_VIEW(1), RECOMMEND_TITLE_VIEW(2), REGISTER_HOBBIES_VIEW(3), RECOMMEND_GATHERING_VIEW(4);
 
     companion object {
         fun valueOf(value: Int): HomeViewType {
