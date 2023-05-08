5e562ab15cd7ff344784cb08b82319b0dccd58cb
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-05-08 16:20:50 +0900

    refactor-202 : PR 코멘트 반영

--- presentation/src/main/java/com/plub/presentation/util/PlubToast.kt
+++ presentation/src/main/java/com/plub/presentation/util/PlubToast.kt
@@ -11,7 +11,7 @@ import com.plub.presentation.databinding.LayoutToastBinding
 
 object PlubToast {
 
-    fun createToast(type : ToastType, context: Context, message: String, length : Int): Toast? {
+    fun createToast(type : ToastType, context: Context, message: String, length : Int = Toast.LENGTH_LONG): Toast {
         val inflater = LayoutInflater.from(context)
         val binding: LayoutToastBinding =
             DataBindingUtil.inflate(inflater, R.layout.layout_toast, null, false)
@@ -21,6 +21,24 @@ object PlubToast {
             ToastType.ERROR -> binding.imageViewIcon.setImageResource(R.drawable.ic_circle_error)
         }
         binding.textViewMessage.text = message
+
+        return Toast(context).apply {
+            setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 16.px)
+            duration = length
+            view = binding.root
+        }
+    }
+
+    fun createToast(type : ToastType, context: Context, message: Int, length : Int = Toast.LENGTH_LONG): Toast {
+        val inflater = LayoutInflater.from(context)
+        val binding: LayoutToastBinding =
+            DataBindingUtil.inflate(inflater, R.layout.layout_toast, null, false)
+
+        when(type){
+            ToastType.COMPLETE -> binding.imageViewIcon.setImageResource(R.drawable.ic_circle_check)
+            ToastType.ERROR -> binding.imageViewIcon.setImageResource(R.drawable.ic_circle_error)
+        }
+        binding.textViewMessage.setText(message)
 
         return Toast(context).apply {
             setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 16.px)
