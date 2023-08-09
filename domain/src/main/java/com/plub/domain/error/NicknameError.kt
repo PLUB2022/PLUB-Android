0e13681409a1d1ed6bd2a98cf9feb7cff502f63b
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-08-09 13:46:56 +0900

    design : 카테고리 선택 디자인 수정

--- domain/src/main/java/com/plub/domain/error/NicknameError.kt
+++ domain/src/main/java/com/plub/domain/error/NicknameError.kt
@@ -5,11 +5,13 @@ sealed class NicknameError : IndividualError() {
     companion object {
         private const val DUPLICATED_NICKNAME = 2010
         private const val LIMIT_CHANGE_NICKNAME = 2080
+        private const val WRONG_VALUE = 2050
 
         fun make(statusCode: Int): NicknameError {
             return when (statusCode) {
                 DUPLICATED_NICKNAME -> DuplicatedNickname("중복 닉네임")
                 LIMIT_CHANGE_NICKNAME -> LimitChangeNickname("닉네임 변경 1회 초과")
+                WRONG_VALUE -> HasSpecialCharacter("잘못된 값 입력")
                 else -> Common
             }
         }
